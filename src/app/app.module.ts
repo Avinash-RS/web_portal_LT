040b8ee1f2ced68e9c323460ff21238328d8f41a
Mythreyi G <MYTHREYI@lntecc.com>
2020-03-10 10:23:36 +0530

    initcommot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 // import { GraphqlModule } from ''
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
