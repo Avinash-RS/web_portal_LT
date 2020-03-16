f4ba52613fe10ac29c4a420c3ea7f6b681a2187c
ankit <b-ankit@lntecc.com>
2020-03-16 16:43:28 +0530

    commit with email validation

--- src/app/learner/pages/registration/registration.component.ts
+++ src/app/learner/pages/registration/registration.component.ts
@@ -30,7 +30,7 @@ export class RegistrationComponent implements OnInit {
   ngOnInit() {
       this.registerForm = this.formBuilder.group({
           username: new FormControl('', [Validators.required, Validators.pattern(/^[A-Za-z ]+$/), Validators.minLength(3), Validators.maxLength(50)]),
-          email: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(64), Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]),
+          email: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(64), Validators.pattern(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/)]),
           termsandconditions: new FormControl("", [])
       }, {
       });
