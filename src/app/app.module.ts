031f479e31eff6725235e851c8956a65ef5c49f0
mythreyi <mythreyi@gmail.com>
2020-03-16 11:35:29 +0530

    created seperate module for angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,12 @@
+//angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
-import { CookieService } from 'ngx-cookie-service';
-import { NgxSpinnerModule } from 'ngx-spinner';
-//mat
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatCardModule } from '@angular/material/card';
-import { MatProgressSpinnerModule, } from '@angular/material/progress-spinner';
-import { MatTooltipModule } from '@angular/material/tooltip';
+//others
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { CookieService } from 'ngx-cookie-service';
 //local
+import { MaterialModule } from './common/material.module';
 import { GraphqlModule } from './graphql/graphql.modules';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,29 +18,14 @@ import { AlertComponentComponent } from './common/alert-component/alert-componen
     AlertComponentComponent,
   ],
   imports: [
+    GraphqlModule,
+    MaterialModule,
     BrowserAnimationsModule,
     BrowserModule,
     NgxSpinnerModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    GraphqlModule,
-
-    MatCardModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatGridListModule,
-    MatTooltipModule,
-    MatDialogModule,
-    MatProgressSpinnerModule, MatRadioModule,
-    MatCardModule,
-    // MatDatepickerModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatRadioModule,
-    MatSelectModule,
   ],
-  providers: [CookieService, AlertComponentComponent],
+  providers: [CookieService,AlertComponentComponent],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   entryComponents: [AlertComponentComponent]
--- src/app/learner/learner.module.ts
+++ src/app/learner/learner.module.ts
@@ -5,24 +5,15 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 //local
-import { GraphqlModule } from './../graphql/graphql.modules';
+
 import { LoginComponent } from './../learner/pages/login/login.component';
 import { CoursedetailsComponent } from './../learner/pages/coursedetails/coursedetails.component';
 import { RegistrationComponent } from './../learner/pages/registration/registration.component';
 import { OtpComponent } from './../learner/pages/otp/otp.component';
 import { PasswordComponent } from './../learner/pages/password/password.component';
+import { MaterialModule } from './../common/material.module';
 //others
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatCardModule } from '@angular/material/card';
-import {MatProgressSpinnerModule,} from '@angular/material/progress-spinner';
-import {MatTooltipModule} from '@angular/material/tooltip';
+
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -33,28 +24,22 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [LoginComponent,CoursedetailsComponent,RegistrationComponent,OtpComponent,PasswordComponent],
+  declarations: [
+    LoginComponent,
+    CoursedetailsComponent,
+    RegistrationComponent,
+    OtpComponent,
+    PasswordComponent],
+
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    MaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    GraphqlModule,
-
-    MatCardModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatGridListModule,
-    MatTooltipModule,
-    MatDialogModule,
-    MatProgressSpinnerModule, MatRadioModule,
-    MatCardModule,
-    // MatDatepickerModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatRadioModule,
-    MatSelectModule,
-  ]
+  ],
+  providers: [],
 })
 export class LearnerModule { }
