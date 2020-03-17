9afdad94584efd13a2c2bd6da666bcbf08f3bf49
mythreyi <mythreyi@gmail.com>
2020-03-17 11:15:18 +0530

    login

--- src/app/learner/learner.module.ts
+++ src/app/learner/learner.module.ts
@@ -1,9 +1,11 @@
 // angular imports
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+// import { CookieService } from 'ngx-cookie-service';
+// import { CookieModule, CookieService } from 'ngx-cookie';
 //local
 
 import { LoginComponent } from './../learner/pages/login/login.component';
@@ -13,7 +15,6 @@ import { OtpComponent } from './../learner/pages/otp/otp.component';
 import { PasswordComponent } from './../learner/pages/password/password.component';
 import { MaterialModule } from './../common/material.module';
 //others
-
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -31,9 +32,10 @@ const routes: Routes = [
     OtpComponent,
     PasswordComponent],
 
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA],
 
   imports: [
+    // CookieModule.forRoot() ,
     CommonModule,
     RouterModule.forChild(routes),
     MaterialModule,
--- src/app/learner/pages/login/login.module.ts
+++ src/app/learner/pages/login/login.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { LoginRoutingModule } from './login-routing.module';
-
+// import { CookieService } from 'ngx-cookie-service';
+// import { CookieModule, CookieService } from 'ngx-cookie';
 @NgModule({
   declarations: [],
   imports: [
+    // CookieModule.forRoot(),
     CommonModule,
-    LoginRoutingModule
-  ]
+    LoginRoutingModule,
+  ],providers : []
 })
 export class LoginModule { }
