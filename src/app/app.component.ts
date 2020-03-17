527b5e363d783c2d201e82317ca8a7373560c7eb
mythreyi <mythreyi@gmail.com>
2020-03-17 11:14:25 +0530

    uninstalled cookie npm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, HostListener } from '@angular/core';
 import { Router, NavigationEnd, NavigationStart } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
-import { HttpClient  } from '@angular/common/http';
+// import { CookieService } from 'ngx-cookie-service';
+import { HttpClient } from '@angular/common/http';
+// import { CookieModule, CookieService } from 'ngx-cookie';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,51 +10,37 @@ import { HttpClient  } from '@angular/common/http';
 })
 export class AppComponent {
   ipAddress = '';
-  constructor(private router: Router, 
-    private cookieService: CookieService,
-    private http:HttpClient
-    ) {
-
-      this.getIPAddress();
-    // this.router.events.subscribe((e) => {
-    // if (e instanceof NavigationStart) {
-    //   console.log('hiiiiiiiiiiii');
-    //   var name = this.cookieService.get('uname') ? this.cookieService.get('uname') : null;
-    //   var ps = this.cookieService.get('ps') ? this.cookieService.get('ps') : null;
-    //   var psd = ps && atob(ps);
-    //   console.log(name,ps,psd)
-    //   if ((name && psd) == null) {
-    //     this.router.navigate(["/login"]);
-    //   }
-    // }
-    // });
+  constructor(private router: Router,
+    // private cookieService: CookieService,
+    private http: HttpClient
+  ) {
+    this.getIPAddress();
   }
 
   ngOnInit() {
     this.getIPAddress();
-}
+  }
 
-  getIPAddress()
-  {
-    this.http.get("http://api.ipify.org/?format=json").subscribe((res:any)=>{
+  getIPAddress() {
+    this.http.get("http://api.ipify.org/?format=json").subscribe((res: any) => {
       this.ipAddress = res.ip;
-      localStorage.setItem('Systemip',this.ipAddress)
+      localStorage.setItem('Systemip', this.ipAddress)
     });
   }
+
   changeOfRoutes() {
-    var name = this.cookieService.get('uname') ? this.cookieService.get('uname') : null;
-    var ps = this.cookieService.get('ps') ? this.cookieService.get('ps') : null;
-    var psd = ps && atob(ps);
-    var cookie = this.cookieService.get('remember_me') ? this.cookieService.get('remember_me') : 'false';
-    console.log(name, ps, psd,(name || psd) == null )
-    if(cookie == 'true') {
-      if ((name || psd) == null) {
-        this.router.navigate(["/login"]);
-      } else {
-        this.router.navigate(["/courses"]);
-      }
-    }
-   
+    // var name = this.cookieService.get('uname') ? this.cookieService.get('uname') : null;
+    // var ps = this.cookieService.get('ps') ? this.cookieService.get('ps') : null;
+    // var psd = ps && atob(ps);
+    // var cookie = this.cookieService.get('remember_me') ? this.cookieService.get('remember_me') : 'false';
+    // console.log(name, ps, psd, (name || psd) == null)
+    // if (cookie == 'true') {
+    //   if ((name || psd) == null) {
+    //     this.router.navigate(["/login"]);
+    //   } else {
+    //     this.router.navigate(["/courses"]);
+    //   }
+    // }
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,11 @@
 //angular
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatCardModule } from '@angular/material/card';
-import {MatProgressSpinnerModule,} from '@angular/material/progress-spinner';
-import {MatTooltipModule} from '@angular/material/tooltip';
-// import { FlexLayoutModule } from '@angular/flex-layout';
 //others
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { CookieService } from 'ngx-cookie-service';
+// import { CookieModule, CookieService } from 'ngx-cookie';
+// import { CookieService } from 'ngx-cookie-service';
 //local
 import { MaterialModule } from './common/material.module';
 import { GraphqlModule } from './graphql/graphql.modules';
@@ -30,6 +19,7 @@ import { AlertComponentComponent } from './common/alert-component/alert-componen
     AlertComponentComponent,
   ],
   imports: [
+    // CookieModule.forRoot() ,
     GraphqlModule,
     MaterialModule,
     BrowserAnimationsModule,
@@ -37,25 +27,11 @@ import { AlertComponentComponent } from './common/alert-component/alert-componen
     NgxSpinnerModule,
     AppRoutingModule,
     GraphqlModule,
-    MatCardModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatGridListModule,
-    // FormsModule,
-    MatDialogModule,
-    MatProgressSpinnerModule, MatRadioModule,
-    MatCardModule,
-    // MatDatepickerModule,
-    // FlexLayoutModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatRadioModule,
-    MatSelectModule,
   ],
-  providers: [CookieService,AlertComponentComponent],
+  providers: [AlertComponentComponent],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA],
   entryComponents: [AlertComponentComponent]
 })
 export class AppModule { }
