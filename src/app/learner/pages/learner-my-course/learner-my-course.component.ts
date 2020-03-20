558213c4a21d914f6ec5dc71818487a36a8ccc92
mythreyi <mythreyi@gmail.com>
2020-03-20 13:43:02 +0530

    wishlist icon added

--- src/app/learner/pages/learner-my-course/learner-my-course.component.ts
+++ src/app/learner/pages/learner-my-course/learner-my-course.component.ts
@@ -14,7 +14,7 @@ export class LearnerMyCourseComponent implements OnInit {
       {
         text: 'Start', cols: 65671, rows: 1565, rating: 2, img: "../../../../assets/learner/1.jpg",
         title : 'Lorem ipsum dolor sit amet,  reprehenderi in voluptate Lorem',
-        description: 'Lorem ipsum dolor sit amet,  reprehenderit in voluptate Lorem ipsum dolor sit amet,  reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur.'
+        description: 'Lorem ipsum dolor sit amet,  reprehenderit in voluptate Lorem ipsum dolor sit amet reprehenderit in voluptate Lorem ipsum dolor sit amet,  reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur.'
       },
       {
         text: 'Resume', cols: 15765, rows: 1565, rating: 5, img: "../../../../assets/learner/2.jpg", statusValue: 70,
@@ -24,12 +24,12 @@ export class LearnerMyCourseComponent implements OnInit {
       {
         text: 'Completed', cols: 65671, rows: 1567, rating: 3, img: "../../../../assets/learner/3.jpg",
         title : 'Lorem ipsum,  reprehenderi in voluptate Lorem',
-        description: 'Lorem ipsum dolor sit amet,  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+        description: 'Lorem ipsum dolor sit amet, reprehenderit in voluptate Lorem  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
       },
       {
         text: 'Completed', cols: 65671, rows: 1567, rating: 1, img: "../../../../assets/learner/4.jpg",
         title : 'Lorem ipsum dolor sit amet',
-        description: 'Lorem ipsum dolor sit amet,  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+        description: 'Lorem ipsum dolor sit amet,  reprehenderit in voluptate Lorem ipsum dolor sit amet reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
       },
     ];
   }
