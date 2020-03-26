b6f3cdaca37a9f3215f6d8c12a89d71d35118391
ankit <b-ankit@lntecc.com>
2020-03-26 10:36:40 +0530

    commit with otp bug fixed

--- src/app/learner/pages/otp/otp.component.ts
+++ src/app/learner/pages/otp/otp.component.ts
@@ -26,7 +26,7 @@ export class OtpComponent implements OnInit {
   systemip:String;
   otp: any;
   showotp: boolean = false;
-  isenable:boolean = false;
+  isenable:boolean = true;
   showverify: boolean = false;
   constructor(private router:Router,
       private formBuilder: FormBuilder,
@@ -69,7 +69,7 @@ get f() { return this.otpForm.controls; }
           if (data.data['user_registration_mobile_otp_send']['success'] == 'true') {
             this.loader.hide();
             this.alert.openAlert(data.data['user_registration_mobile_otp_send']['message'],null)
-            this.isenable = true;
+            this.isenable = false;
             this.showotp = true;
           } 
       })
