d78a68d99f0639cf7e6c2c92096c7468fcfea3c0
Senthil Kumar <senthsk@lntecc.com>
2020-04-03 10:53:38 +0000

    Updated sampleUnitTest.js

--- Tests/unit_tests/sampleUnitTest.js
+++ Tests/unit_tests/sampleUnitTest.js
@@ -1,8 +1,35 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+
+var http = require('http');
+ 
+describe('sampleUnitTests', function () {
+    before(function () {
     });
-  });
+  
+    after(function () {
+    
+    });
+
+    it('Should return 200', function (done) {
+        http.get('http://localhost:3000', function (res) {
+            assert.equal(200, res.statusCode, 'Result code should be 200.');
+            done();
+        });
+    });
+
+    it('Assert title', function (done) {
+        http.get('http://localhost:3000', function (res) {
+            assert.equal(200, res.statusCode, 'Result code should be 200.');
+            var data = '';
+
+            res.on('data', function (chunk) {
+                data += chunk;
+            });
+
+            res.on('end', function () {
+                assert.equal(true, data.includes('<title>Express - Node.js Express Application</title>'), 'Title should be Express - Node.js Express Application.');
+                done();
+            })
+        });
+    });
 });
