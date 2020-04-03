d6f0f8414b8e8b242e1fe44687fd656d160bb726
Senthil Kumar <senthsk@lntecc.com>
2020-04-03 09:47:16 +0000

    Updated unitTest.js

--- Tests/unit_tests/unitTest.js
+++ Tests/unit_tests/unitTest.js
@@ -9,26 +9,5 @@ describe('sampleUnitTests', function () {
     after(function () {
         
     });
-
-    it('Should return 200', function (done) {
-        http.get('http://localhost:3000', function (res) {
-            
-        });
-    });
-
-    it('Assert title', function (done) {
-        http.get('http://localhost:3000', function (res) {
-           
-            var data = '';
-
-            res.on('data', function (chunk) {
-                data += chunk;
-            });
-
-            res.on('end', function () {
-               
-                done();
-            })
-        });
-    });
-});
+  
+ });
