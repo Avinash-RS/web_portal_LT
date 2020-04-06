b648fad644c970f19961bd1321791b830210506e
mythreyi <mythreyi.gullapalli@cintanatech.com>
2020-04-06 15:58:46 +0530

    landing page

--- src/app/core/shared/wishlist-courses/wishlist-courses.component.ts
+++ src/app/core/shared/wishlist-courses/wishlist-courses.component.ts
@@ -11,7 +11,7 @@ export class WishlistCoursesComponent implements OnInit {
   wishlist: any = [];
   @Input('from') from: any;
   breakpoint: number;
-  height_row: number;
+
   constructor(public service: CommonServicesService, private gs: GlobalServiceService, ) { }
 
   ngOnInit() {
@@ -21,46 +21,28 @@ export class WishlistCoursesComponent implements OnInit {
         this.viewWishlist()
       )
     }
-    if (window.innerWidth <= 480) { 
-      this.breakpoint = 1; 
-      this.height_row = 5;
-    }
-
-    else if (window.innerWidth >= 480 && window.innerWidth <= 768) { 
-      this.breakpoint = 2; 
-      this.height_row = 1.5;
-    }
-
-    else if (window.innerWidth >= 768 && window.innerWidth <= 992) { this.breakpoint = 3; 
-      this.height_row = 1;}
-
+    if (window.innerWidth <= 480)
+      this.breakpoint = 1;
+    else if (window.innerWidth >= 480 && window.innerWidth <= 768)
+      this.breakpoint = 2;
+    else if (window.innerWidth >= 768 && window.innerWidth <= 992) this.breakpoint = 3;
     // else if (window.innerWidth >= 992 && window.innerWidth <= 1200)
     //   this.breakpoint = 4;
-    else {
+    else
       this.breakpoint = 4;
-    }
   }
 
   onResize(event) {
-    if (event.target.innerWidth <= 480) {
+    if (event.target.innerWidth <= 480)
       this.breakpoint = 1;
-      this.height_row = 2;
-    }
-    else if (event.target.innerWidth >= 480 && event.target.innerWidth <= 768) {
+    else if (event.target.innerWidth >= 480 && event.target.innerWidth <= 768)
       this.breakpoint = 2;
-      this.height_row = 1.5;
-    }
-    else if (event.target.innerWidth >= 768 && event.target.innerWidth <= 992) {
+    else if (event.target.innerWidth >= 768 && event.target.innerWidth <= 992)
       this.breakpoint = 3;
-      this.height_row = 1;
-    }
     // else if (event.target.innerWidth >= 992 && event.target.innerWidth <= 1200)
     //   this.breakpoint = 4;
-    else {
+    else
       this.breakpoint = 4;
-    }
-
-
   }
 
   viewWishlist() {
