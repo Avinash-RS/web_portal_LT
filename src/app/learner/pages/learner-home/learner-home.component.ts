cea5c050a1280947895cf178fe1224636afdb9bd
ankit <b-ankit@lntecc.com>
2020-04-06 18:23:27 +0530

    commit with header and strams add in home page

--- src/app/learner/pages/learner-home/learner-home.component.ts
+++ src/app/learner/pages/learner-home/learner-home.component.ts
@@ -11,19 +11,24 @@ import { map, filter } from 'rxjs/operators';
   styleUrls: ['./learner-home.component.scss']
 })
 export class LearnerHomeComponent implements OnInit {
+
   tiles: any = [];
   userDetailes: any;
   panelOpenState = false;
   state$: any;
+  breakpoint: number;
 
   constructor(public service: LearnerServicesService, private router: Router, private gs: GlobalServiceService,
     private loader: Ng4LoadingSpinnerService, public activatedRoute: ActivatedRoute) { 
     }
   ngOnInit() {
+      this.breakpoint = (window.innerWidth <= 400) ? 1 : 4;
       this.userDetailes =JSON.parse(localStorage.getItem('UserDetails')) || null;
   }
   myCourses() {
 
   }
-
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= 400) ? 1 : 4;
+  }
 }
