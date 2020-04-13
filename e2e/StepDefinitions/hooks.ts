d83b534fb9f3ec60665970672c3199d1f9933573
Tamil Selvan J <TAMIL-J@lntecc.com>
2020-04-13 10:35:42 +0000

    Updated 9 files under /e2e/StepDefinitions

---
+++ e2e/StepDefinitions/hooks.ts
@@ -0,0 +1,23 @@
+
+import {After,Before, Status} from "cucumber";
+import { browser } from "protractor";
+
+Before(async function () {
+    console.log("Test Started")
+  });
+
+After(async function(scenario) {
+  // This hook will be executed before scenarios tagged with @foo
+  console.log("Test is completed");
+  if (scenario.result.status=== Status.FAILED)
+  {
+    //code to take screesnhot
+   const screenshot= await browser.takeScreenshot(); 
+   this.attach(screenshot,"image/png");
+  }
+});
+
+
+
+
+
