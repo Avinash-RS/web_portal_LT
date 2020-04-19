616e75a99559ddb4a2be25aa8b01dfbc9697daae
Rajesh K <rajesh-trl@lntecc.com>
2020-04-19 22:07:34 +0530

    change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,7 +27,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,{
-    scrollPositionRestoration: 'enabled', // Add options right here
+    scrollPositionRestoration: 'enabled', 
   })],
   exports: [RouterModule]
 })
