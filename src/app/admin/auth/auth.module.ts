f4a0bf02eea3308b4afa27aa25fc1614484486e0
Afser Ali <afser-ali@lntecc.com>
2020-04-19 21:09:28 +0530

    Update auth.module.ts

--- src/app/admin/auth/auth.module.ts
+++ src/app/admin/auth/auth.module.ts
@@ -4,17 +4,14 @@ import { AuthComponent } from './auth.component';
 import { UserManagementComponent} from '../pages/user-management/user-management.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule,ReactiveFormsModule  } from '@angular/forms';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTabsModule, MatMenuModule, MatInputModule, MatCheckboxModule, MatFormFieldModule, MatCardModule, MatGridListModule, MatSelectModule, MatRadioModule, MatDialogModule, MatTooltipModule,  MatProgressBarModule, MatAutocompleteModule, MatExpansionModule, MatTableModule, MatPaginatorModule } from '@angular/material';
-
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
-
 import { appRoutes } from './lazyloader.routes';
 
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
