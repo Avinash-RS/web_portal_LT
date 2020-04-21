a9009ebf5a194863c8c0126f6965db8811b8d9dd
ankit <b-ankit@lntecc.com>
2020-04-21 19:56:50 +0530

    commite with course player details api intergated

--- src/app/learner/pages/scormplayer/scormplayer.component.ts
+++ src/app/learner/pages/scormplayer/scormplayer.component.ts
@@ -3,6 +3,11 @@ import { LearnerServicesService } from '../../services/learner-services.service'
 import { ActivatedRoute, Router } from '@angular/router';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { environment } from '../../../../environments/environment';
+import { CommonServicesService } from '@core/services/common-services.service';
+import { AlertServiceService } from '@core/services/handlers/alert-service.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+
 @Component({
   selector: 'app-scormplayer',
   templateUrl: './scormplayer.component.html',
@@ -13,15 +18,17 @@ export class ScormplayerComponent implements OnInit {
   content: any;
   syllabus: any;
   modules: any [];
+  authorDetails: any [];
   name = 'Set iframe source';
   url: string 
   urlSafe: SafeResourceUrl;
   user_id:any
   breakpoint: number;
   course_id:any;
+  courseDeatils: any;
 
-  constructor(public sanitizer: DomSanitizer,public activatedRoute: ActivatedRoute, 
-    public service: LearnerServicesService, public route: Router) { 
+  constructor(public sanitizer: DomSanitizer,    public spinner: NgxSpinnerService,public activatedRoute: ActivatedRoute,  private alert: AlertServiceService,
+    public service: LearnerServicesService, public route: Router,public commonService : CommonServicesService,) { 
       var detail = (this.route.getCurrentNavigation() && this.route.getCurrentNavigation().extras && 
       this.route.getCurrentNavigation().extras.state && this.route.getCurrentNavigation().extras.state.detail);
       console.log(detail)
@@ -30,6 +37,19 @@ export class ScormplayerComponent implements OnInit {
       this.course_id=detail.course_id
       localStorage.setItem('scorm_user_id',this.user_id)
       localStorage.setItem('course_id',this.course_id)
+      this.spinner.show();
+      this.commonService.viewCurseByID (this.course_id).subscribe(data => {
+        if(data.data['viewcourse'].success == true){
+          this.courseDeatils = data.data['viewcourse']['message'][0];
+          console.log(this.courseDeatils )
+          this.authorDetails  =  this.courseDeatils.author_details;
+          this.spinner.hide();
+        }else{
+          this.spinner.hide();
+          this.alert.openAlert(data.data['viewcourse']['error_msg'],null)
+        }
+       
+      })
     }
 
   ngOnInit() {
@@ -51,6 +71,7 @@ export class ScormplayerComponent implements OnInit {
       if(this.content&&this.content.syllabus_of_particular_scorm&&this.content.syllabus_of_particular_scorm.success){
         this.syllabus = this.content.syllabus_of_particular_scorm.data[0].scorm_dtl_user_map
         this.modules = this.syllabus[0].children;
+        console.log( this.modules)
       }else{
         console.log('no record')
       }
