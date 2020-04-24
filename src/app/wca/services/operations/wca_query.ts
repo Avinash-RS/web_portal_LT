c53e1c33d92bd85c53350bf86041dc726589d22c
RAJESH-KUMAR-RANJAN <mv3vfn6zm7we5aqmygsobz4zpsbiprwmgq7kgqmlr4rhkfbwgbpq>
2020-04-23 21:09:59 -0700

    Update wca_query.ts

--- src/app/wca/services/operations/wca_query.ts
+++ src/app/wca/services/operations/wca_query.ts
@@ -2,14 +2,14 @@ import gql from "graphql-tag";
 
 export const remove_doc_ref = gql`
   query remove_doc_ref($doc_id: String){
-    login(doc_id: $doc_id ) {
+    remove_doc_ref(doc_id: $doc_id ) {
         message
         success
     }
   }`;
 
   export const getallrefdoc = gql`
-  query remove_doc_ref{
+  query getallrefdoc{
     getallrefdoc {
         message{
             _id
