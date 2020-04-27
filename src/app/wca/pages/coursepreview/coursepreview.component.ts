b454bc73add784b9f705e2b3d2a10e774716d81e
ankit <b-ankit@lntecc.com>
2020-04-27 17:22:00 +0530

    commite

--- src/app/wca/pages/coursepreview/coursepreview.component.ts
+++ src/app/wca/pages/coursepreview/coursepreview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonServicesService } from '@core/services/common-services.service';
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+import { LearnerServicesService } from '@learner/services/learner-services.service';
 
 @Component({
   selector: 'app-coursepreview',
@@ -25,8 +26,9 @@ export class CoursepreviewComponent implements OnInit {
     nav: true
   }
   course: any;
-  constructor( public service: CommonServicesService,private loader: Ng4LoadingSpinnerService,) {
-
+  content: any;
+  constructor( public service: CommonServicesService, public learnerservice: LearnerServicesService,private loader: Ng4LoadingSpinnerService,) {
+   
     this.service.viewCurseByID(1).subscribe((viewCourse: any) => {
           console.log(viewCourse,"detail")
       if (viewCourse.data.viewcourse && viewCourse.data.viewcourse.success) {
@@ -39,9 +41,23 @@ export class CoursepreviewComponent implements OnInit {
    }
 
   ngOnInit() {
+    this.getModuleData()
   }
   clickedT(i) {
     this.clicked = i
   }
-
+  getModuleData() {
+    this.learnerservice.getModuleData(1).subscribe(data => {
+      console.log(data)
+      // if(data.data['getmoduleData']['success'] == true){
+      
+        this.content = data.data['getmoduleData']['data'][0]
+        console.log( this.content,"contenyt")
+      // }
+     
+      // if(this.content&&this.content.getModuleData&&this.content.getModuleData.success){
+      //    this.content = this.content.getModuleData.data[0]
+      // }   
+    })
+  }
 }
