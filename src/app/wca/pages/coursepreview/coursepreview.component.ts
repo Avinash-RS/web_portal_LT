19fcbf05593d44f531dbf9703fbfeb068626ce19
ankit <b-ankit@lntecc.com>
2020-05-02 18:51:42 +0530

    commit with dynamic course_id

--- src/app/wca/pages/coursepreview/coursepreview.component.ts
+++ src/app/wca/pages/coursepreview/coursepreview.component.ts
@@ -37,6 +37,8 @@ export class CoursepreviewComponent implements OnInit {
     this.detail = (this.route.getCurrentNavigation() && this.route.getCurrentNavigation().extras &&
       this.route.getCurrentNavigation().extras.state && this.route.getCurrentNavigation().extras.state.detail);
     this.loader.show();
+
+    console.log(this.detail,'course_id')
     this.courseType = localStorage.getItem('courseType')
     if (this.courseType === "create") {
       this.isshowPublish = true
@@ -44,7 +46,7 @@ export class CoursepreviewComponent implements OnInit {
       this.isshowPublish = false
     }
 
-    this.service.viewCurseByID(1).subscribe((viewCourse: any) => {
+    this.service.viewCurseByID(this.detail.id).subscribe((viewCourse: any) => {
       if (viewCourse.data.viewcourse && viewCourse.data.viewcourse.success) {
         this.course = viewCourse.data.viewcourse.message[0];
         console.log(this.course, 'course')
@@ -79,7 +81,7 @@ export class CoursepreviewComponent implements OnInit {
     this.clicked = i
   }
   getModuleData() {
-    this.learnerservice.getModuleData(1).subscribe(data => {
+    this.learnerservice.getModuleData(this.detail.id).subscribe(data => {
       console.log(data)
       // if(data.data['getmoduleData']['success'] == true){
 
@@ -107,4 +109,29 @@ export class CoursepreviewComponent implements OnInit {
     this.breakpoint = (window.innerWidth <= 400) ? 1 : 2;
 
   }
+
+  downloadAll(urls) {
+    //  console.log(urls)
+    //  var arr: any = [];
+    //  debugger
+    //  urls.forEach(element => {
+    //    console.log(element.path)
+    //    arr.push(element.path);
+ 
+    //   });
+    //    var link = document.createElement('a');
+    //    link.setAttribute('download', null);
+    //    link.style.display = 'none';
+    //    document.body.appendChild(link);
+     
+    //    for (var i = 0; i < arr.path.lenth; i++) {
+         
+    //      link.setAttribute('href', arr[i]);
+    //      console.log(arr[i],'arr[i]')
+    //      link.click();
+    //    }
+    //    document.body.removeChild(link); 
+    
+
+}
 }
