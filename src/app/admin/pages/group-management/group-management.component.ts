b746ee136fc28a80fd636d811f453628209fb6f2
Afser-Ali <AFSER-ALI@lntecc.com>
2020-05-06 17:23:46 +0530

    user changes for group

--- src/app/admin/pages/group-management/group-management.component.ts
+++ src/app/admin/pages/group-management/group-management.component.ts
@@ -254,6 +254,7 @@ export class GroupManagementComponent implements OnInit {
             this.editstatus = true;
             this.currentpath = null;
             this.editgroupname = '';
+            this.disabled = true;
             this.getgroups();
             this.cdr.detectChanges();
             Swal.fire(
@@ -381,7 +382,7 @@ export class GroupManagementComponent implements OnInit {
   }
 
   tabClick(event) {
-    if (event.index === 1) {
+    if (event.index === 1 && this.currentpath) {
       const pagenumber = 0;
       this.getAllUser(pagenumber);
     }
