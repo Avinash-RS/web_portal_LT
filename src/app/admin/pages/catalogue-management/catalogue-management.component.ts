005e4289c06ce31bed857de5f74aa01cdf470130
Kanimozhiharish18 <57133205+Kanimozhiharish18@users.noreply.github.com>
2020-05-12 18:27:27 +0530

    Text changes

--- src/app/admin/pages/catalogue-management/catalogue-management.component.ts
+++ src/app/admin/pages/catalogue-management/catalogue-management.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GlobalServiceService } from '@core/services/handlers/global-service.service';
 import { AlertServiceService } from '@core/services/handlers/alert-service.service';
 import { AdminServicesService } from '@admin/services/admin-services.service';
+import { LearnerServicesService } from '@learner/services/learner-services.service';
 import { Router } from '@angular/router';
 import { FormBuilder } from '@angular/forms';
 
@@ -12,9 +13,11 @@ import { FormBuilder } from '@angular/forms';
 })
 export class CatalogueManagementComponent implements OnInit {
   adminDetails: any;
+  // userDetailes: any;
+  // allcourses: any;
 
-  constructor(private gs: GlobalServiceService, private alert: AlertServiceService, private adminservice: AdminServicesService,
-    private formBuilder: FormBuilder, private router: Router) {
+
+  constructor(private gs: GlobalServiceService, private alert: AlertServiceService, private adminservice: AdminServicesService, public learnerservice: LearnerServicesService, private formBuilder: FormBuilder, private router: Router) {
     this.adminDetails = this.gs.checkLogout();
     console.log(this.adminDetails)
   }
@@ -23,4 +26,13 @@ export class CatalogueManagementComponent implements OnInit {
 
   }
 
+
+  // gotoedit() {
+  //   console.log(this.userDetailes.group_id[0])
+  //   this.learnerservice.getallcourses(this.userDetailes.group_id[0], this.pagenumber).subscribe((result: any) => {
+  //     this.allcourses = result.data.get_all_course_by_usergroup.message;
+  //   });
+  // }
+
+  // 5eb3b5f50d03e1bc320162cd id 
 }
