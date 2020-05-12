4237ea2b7c52272557e981cdc8c5c3142256e682
Senthil Kumar <senthsk@lntecc.com>
2020-05-12 06:10:53 +0000

    Updated environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,10 +10,6 @@ export const environment = {
   apiUrlImg:'http://40.76.47.212:3000/',
   createCourseApi:'http://40.76.47.212:3002/',
   wcaapiurl:'http://edutech.eastus.cloudapp.azure.com/',
-    // apiUrl: 'http://20.44.41.200:3000/',
-  // scormUrl:'http://20.44.41.200:8080/',
-  // apiUrlImg:'http://20.44.41.200:3001/',
-  // wcaapiurl:'http://20.44.41.200:9001/'
 };
 
 /*
