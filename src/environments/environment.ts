63d0bb23fda779474ca1c7d81734148543bcbb7a
Afser Ali <afser-ali@lntecc.com>
2020-05-12 21:29:43 +0530

    commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
 
   apiUrl: 'http://20.44.41.200:3000/',
   scormUrl:'http://20.44.36.116:8089/',
-  apiUrlImg:'http://40.76.47.212:3000/',
+  apiUrlImg:'http://20.44.41.200:3000/',
   createCourseApi:'http://20.44.41.55:3002/',
   wcaapiurl:'http://20.44.34.169:9001/',
 };
