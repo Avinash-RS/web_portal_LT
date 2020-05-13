d1dd961265672be9a06fdfc3863af25687102307
Afser Ali <afser-ali@lntecc.com>
2020-05-13 23:18:34 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,11 @@
 export const environment = {
   production: false,
 
-  apiUrl: 'http://40.76.47.212:3000/', //old dev now uat
- 
-  // apiUrl: 'http://20.44.41.200:3000/', //new dev
-  scormUrl:'http://40.76.47.212:8080/',
-  apiUrlImg:'http://40.76.47.212:3000/',
-  createCourseApi:'http://40.76.47.212:3002/',
-  wcaapiurl:'http://edutech.eastus.cloudapp.azure.com/',
+    apiUrl: 'http://20.44.41.200:3000/',
+    scormUrl:'http://20.44.36.116:8089/',
+    apiUrlImg:'http://20.44.41.200:3000/',
+    createCourseApi:'http://20.44.41.55:3002/',
+    wcaapiurl:'http://20.44.34.169:9001/', 
     // apiUrl: 'http://20.44.41.200:3000/',
   // scormUrl:'http://20.44.41.200:8080/',
   // apiUrlImg:'http://20.44.41.200:3001/',
