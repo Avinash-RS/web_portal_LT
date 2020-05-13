46eb8168a4ed21cb2759faeea32f25ab51446ec5
vijay pagalavan <pagalavanps@gmail.com>
2020-05-13 12:10:45 +0530

    file upload functionality

--- src/app/wca/pages/view-module/view-module.component.ts
+++ src/app/wca/pages/view-module/view-module.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { WcaService } from '../../services/wca.service';
@@ -12,6 +12,9 @@ export class ViewModuleComponent implements OnInit {
   queryData: any;
   courseDetails: any;
   isFileContent = false;
+  scormCourse: any;
+  scormPath: string;
+  @ViewChild('file') fileUploaded;
   constructor(
     private router: Router,
     public route: ActivatedRoute,
@@ -30,7 +33,6 @@ export class ViewModuleComponent implements OnInit {
       }
       if (flag) {
         this.queryData = params;
-        console.log(this.queryData)
       }
     });
   }
@@ -46,23 +48,38 @@ export class ViewModuleComponent implements OnInit {
     this.router.navigate(['/Admin/auth/Wca/viewmodule'], { queryParams: { viewingModule: this.queryData.viewingModule, image: this.queryData.image, courseName: this.queryData.courseName } });
   }
 
-  uploadDoc(fileList: FileList): void {
+  navModuleRepository() {
+    this.router.navigate(['/Admin/auth/Wca/modulerepository'], { queryParams: { viewingModule: this.queryData.viewingModule, image: this.queryData.image, courseName: this.queryData.courseName } });
+  }
+
+  onUploadDoc(fileList: FileList): void {
     let file = fileList[0];
+    this.scormCourse = '';
     let fileReader: FileReader = new FileReader();
     let that = this;
     that.isFileContent = false;
     fileReader.onloadend = function (x) {
       that.isFileContent = String(fileReader.result).includes("imsmanifest.xml") ? true : false;
       if (!that.isFileContent) {
+        that.fileUploaded.nativeElement.value = '';
         that.toast.warning('Kindly upload a valid zip file');
       }
       else {
-
+        that.uploadDoc(file);
       }
     }
     fileReader.readAsText(file);
   }
 
+  uploadDoc(file) {
+    this.scormCourse = file;
+    const formData = new FormData();
+    formData.append('file', this.scormCourse);
+    this.wcaService.uploadScromCourse(formData).subscribe((data: any) => {
+      this.scormPath = 'https://edutechstorage.blob.core.windows.net/' + data.path;
+      debugger
+    })
+  }
 
 }
 
