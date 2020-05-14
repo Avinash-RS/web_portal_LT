c7147b765f984fb2178383bdfe750b28b95e8d7f
mythreyi <mythreyi.gullapalli@cintanatech.com>
2020-05-14 10:13:38 +0530

    modified test case

--- src/app/admin/admin-login/admin-login.component.spec.ts
+++ src/app/admin/admin-login/admin-login.component.spec.ts
@@ -22,35 +22,36 @@ describe('AdminLoginComponent', () => {
   let fixture: ComponentFixture<AdminLoginComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({ imports: [
-      ReactiveFormsModule,
-      FormsModule,
-      MatButtonModule,
-      MatMenuModule,
-      MatInputModule,
-      MatToolbarModule,
-      MatCheckboxModule,
-      MatFormFieldModule,
-      MatIconModule,
-      MatCardModule,
-      MatGridListModule,
-      MatSelectModule,
-      MatRadioModule,
-      MatDialogModule,
-      MatTooltipModule,
-      HttpClientModule,
-      ApolloModule,
-      RouterModule.forRoot([]),
-      HttpLinkModule],
-    
-    schemas: [
-      CUSTOM_ELEMENTS_SCHEMA,
-      NO_ERRORS_SCHEMA
-    ],
-    providers: [Apollo, AlertComponentComponent],
-      declarations: [ AdminLoginComponent ]
+    TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        FormsModule,
+        MatButtonModule,
+        MatMenuModule,
+        MatInputModule,
+        MatToolbarModule,
+        MatCheckboxModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatCardModule,
+        MatGridListModule,
+        MatSelectModule,
+        MatRadioModule,
+        MatDialogModule,
+        MatTooltipModule,
+        HttpClientModule,
+        ApolloModule,
+        RouterModule.forRoot([]),
+        HttpLinkModule],
+
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA,
+        NO_ERRORS_SCHEMA
+      ],
+      providers: [Apollo, AlertComponentComponent],
+      declarations: [AdminLoginComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -76,10 +77,10 @@ describe('AdminLoginComponent', () => {
 
   it('username field should be valid', () => {
     let errors = {};
-    let user= component.loginForm.controls['username'];
+    let user = component.loginForm.controls['username'];
     expect(user.valid).toBeFalsy();
     errors = user.errors || {};
-    expect(errors['required']).toBeTruthy();    
+    expect(errors['required']).toBeTruthy();
   });
 
   it('password field should be valid', () => {
@@ -87,25 +88,25 @@ describe('AdminLoginComponent', () => {
     let password = component.loginForm.controls['password'];
     errors = password.errors || {};
     expect(errors['required']).toBeTruthy();
-});
+  });
 
-it('signin', () =>{
-  let errors = {}
-  let error = {}
+  it('signin', () => {
+    let errors = {}
+    let error = {}
     let uname = component.loginForm.controls['username'];
     expect(uname.valid).toBeFalsy();
 
     errors = uname.errors || {};
     expect(errors['required']).toBeTruthy();
-   
+
     let password = component.loginForm.controls['password'];
 
-    errors = password.errors|| {};
+    errors = password.errors || {};
     expect(errors['required']).toBeTruthy();
 
     component.login();
     expect(component.login()).toBeTruthy();
-})
+  })
 
 
 });
--- test.ts
+++ test.ts
@@ -21,7 +21,7 @@ getTestBed().initTestEnvironment(
 // const context = require.context('../src/app/learner/pages/login/login.component', true, /\.spec\.ts$/);
 // const context = require.context('./app/admin/admin-login', true, /admin-login.component\.spec\.ts$/);
 // const context = require.context('./app/admin/pages/user-management', true, /user-management.component\.spec\.ts$/);
-// const context = require.context('./app/admin/pages/admin-courses', true, /admin-courses.component\.spec\.ts$/);
-const context = require.context('./app/learner/pages/learner-my-course', true, /admin-courses.component\.spec\.ts$/);
+const context = require.context('./app/admin/pages/admin-courses', true, /admin-courses.component\.spec\.ts$/);
+// const context = require.context('./app/learner/pages/learner-my-course', true, /admin-courses.component\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
