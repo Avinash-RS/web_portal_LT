1461e0636854819c34200934839acf8ca41b21a0
vijay pagalavan <pagalavanps@gmail.com>
2020-05-18 23:17:51 +0530

    tooltip implemented

--- src/app/wca/pages/view-courses/view-courses.component.ts
+++ src/app/wca/pages/view-courses/view-courses.component.ts
@@ -9,18 +9,37 @@ import { MAT_DIALOG_DATA, MatDialog } from '@angular/material';
 export class ViewCoursesComponent implements OnInit {
 
   moduleData;
-  constructor(@Inject(MAT_DIALOG_DATA) public data,public dialog: MatDialog) { }
+  isHover: any;
+  currentTop: any;
+  currentText: any;
+  constructor(@Inject(MAT_DIALOG_DATA) public data, public dialog: MatDialog) { }
 
   ngOnInit() {
     this.moduleData = this.data.module
     this.moduleData.createdon = this.moduleData.createdon ? new Date(this.moduleData.createdon) : '';
-   this.moduleData.coursedetails.forEach((data) => {
-     data.createdon = data.createdon ? new Date(data.createdon) : '';
-   })
-   
+    this.moduleData.coursedetails.forEach((data) => {
+      data.createdon = data.createdon ? new Date(data.createdon) : '';
+    })
+
   }
 
-  closeDialog(){
+  closeDialog() {
     this.dialog.closeAll();
+  }
+
+  onHover(e) {
+
+    if (e.currentTarget.offsetWidth < e.currentTarget.scrollWidth) {
+      this.isHover = true;
+      this.currentText = e.target.outerText;
+      this.currentTop = {
+        top: e.clientY + 10,
+        left: e.clientX
+      }
+    }
+  }
+  onhoverLeave() {
+    this.currentText = '';
+   this.isHover = false;
   }
 }
