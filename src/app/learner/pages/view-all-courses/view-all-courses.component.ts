c5c5a034ddacc5cf38c7fbdcd4ff76bdd91d6f4a
Ramya Thothathiri <ramyathothathiri@lntecc.com>
2020-05-19 20:18:02 +0530

    uncheck in filter completed

--- src/app/learner/pages/view-all-courses/view-all-courses.component.ts
+++ src/app/learner/pages/view-all-courses/view-all-courses.component.ts
@@ -143,12 +143,32 @@ export class ViewAllCoursesComponent implements OnInit {
     if (event.target.checked) {
       if (type === 'language') {
         this.selectedlang.push(val.course_language);
+        this.guidelineSearchVal.course_data.forEach(element => {
+          if(element.course_language === val.course_language){
+            element.checked = true;
+          }
+        });
       } else if (type === 'Instructor') {
         this.authordetails.push(val.authordetails);
+        this.guidelineSearchVal.author_data.forEach(element => {
+          if(element.authordetails === val.authordetails){
+            element.checked = true;
+          }
+        });
       } else if (type === 'courseMode') {
         this.coursemode.push(val.course_mode);
+        this.guidelineSearchVal.coursemode_data.forEach(element => {
+          if(element.course_mode === val.course_mode){
+            element.checked = true;
+          }
+        });
       } else if (type === 'coursepartner') {
         this.coursepartners.push(val.coursepartnerdetails);
+        this.guidelineSearchVal.coursepartner_data.forEach(element => {
+          if(element.coursepartnerdetails === val.coursepartnerdetails){
+            element.checked = true;
+          }
+        });
       }
     }
     else if (!event.target.checked) {
@@ -157,24 +177,44 @@ export class ViewAllCoursesComponent implements OnInit {
         if (index > -1) {
           this.selectedlang.splice(index, 1);
         }
+        this.guidelineSearchVal.course_data.forEach(element => {
+          if(element.course_language === val.course_language){
+            element.checked = false;
+          }
+        });
       }
       else if (type === 'Instructor') {
         let index = this.authordetails.indexOf(val.authordetails);
         if (index > -1) {
           this.authordetails.splice(index, 1);
         }
+        this.guidelineSearchVal.author_data.forEach(element => {
+          if(element.authordetails === val.authordetails){
+            element.checked = false;
+          }
+        });
       }
       else if (type === 'courseMode') {
         let index = this.coursemode.indexOf(val.course_mode);
         if (index > -1) {
           this.coursemode.splice(index, 1);
         }
+        this.guidelineSearchVal.coursemode_data.forEach(element => {
+          if(element.course_mode === val.course_mode){
+            element.checked = false;
+          }
+        });
       }
       else if (type === 'coursepartner') {
         let index = this.coursepartners.indexOf(val.coursepartnerdetails);
         if (index > -1) {
           this.coursepartners.splice(index, 1);
         }
+        this.guidelineSearchVal.coursepartner_data.forEach(element => {
+          if(element.coursepartnerdetails === val.coursepartnerdetails){
+            element.checked = false;
+          }
+        });
       }
     }
 
@@ -196,17 +236,39 @@ export class ViewAllCoursesComponent implements OnInit {
     let authorIndex = this.authordetails.indexOf(val);
     let coursepartnersIndex = this.coursepartners.indexOf(val);
     let coursemodeIndex = this.coursemode.indexOf(val);
+
     if (langIndex > -1) {
       this.selectedlang.splice(langIndex, 1);
+      this.guidelineSearchVal.course_data.forEach(element => {
+        if(element.course_language === val){
+          element.checked = false;
+        }
+      });
     }
+
     else if (authorIndex > -1) {
       this.authordetails.splice(authorIndex, 1);
+      this.guidelineSearchVal.author_data.forEach(element => {
+        if(element.authordetails === val){
+          element.checked = false;
+        }
+      });
     }
     else if (coursepartnersIndex > -1) {
       this.coursepartners.splice(coursepartnersIndex, 1);
+      this.guidelineSearchVal.coursepartner_data.forEach(element => {
+        if(element.coursepartnerdetails === val){
+          element.checked = false;
+        }
+      });
     }
     else if (coursemodeIndex > -1) {
       this.coursemode.splice(coursemodeIndex, 1);
+      this.guidelineSearchVal.coursemode_data.forEach(element => {
+        if(element.course_mode === val){
+          element.checked = false;
+        }
+      });
     }
     this.selectedFilter = [];
     this.selectedFilter = this.selectedFilter.concat(this.selectedlang);
@@ -227,6 +289,26 @@ export class ViewAllCoursesComponent implements OnInit {
     this.authordetails = [];
     this.coursemode = [];
     this.coursepartners = [];
+    if(this.guidelineSearchVal && this.guidelineSearchVal.course_data){
+      this.guidelineSearchVal.course_data.forEach(element => {
+        element.checked = false;
+      });
+    }
+    if(this.guidelineSearchVal && this.guidelineSearchVal.author_data){
+      this.guidelineSearchVal.author_data.forEach(element => {
+        element.checked = false;
+      });
+    }
+    if(this.guidelineSearchVal && this.guidelineSearchVal.coursemode_data){
+      this.guidelineSearchVal.coursemode_data.forEach(element => {
+        element.checked = false;
+      });
+    }
+    if(this.guidelineSearchVal && this.guidelineSearchVal.coursepartner_data){
+      this.guidelineSearchVal.coursepartner_data.forEach(element => {
+        element.checked = false;
+      });
+    }
     var perPage = "10";
     this.pagenumber = 1;
     this.learnerservice.postGuildelineSearchData(this.Lvl1CatId, this.Lvl2CatId, this.Lvl3CatId, this.selectedlang, this.coursemode,
@@ -267,6 +349,30 @@ export class ViewAllCoursesComponent implements OnInit {
         this.learnerservice.getGuidelineSearch().subscribe((result : any)=>{
            if(result['data']['getDetailsCount']['success'] == 'true'){
             this.guidelineSearchVal = result['data']['getDetailsCount']['message'];
+            if(this.guidelineSearchVal && this.guidelineSearchVal.course_data){
+              this.guidelineSearchVal.course_data.forEach(element => {
+                element.checked = false;
+              });
+            }
+            if(this.guidelineSearchVal && this.guidelineSearchVal.author_data){
+            this.guidelineSearchVal.author_data.forEach(element => {
+              element.checked = false;
+            });
+          }
+          if(this.guidelineSearchVal && this.guidelineSearchVal.coursemode_data){
+            this. guidelineSearchVal.coursemode_data.forEach(element => {
+              element.checked = false;
+            });
+          }
+          if(this.guidelineSearchVal && this.guidelineSearchVal.coursepartner_data){
+            this.guidelineSearchVal.coursepartner_data.forEach(element => {
+              element.checked = false;
+            });
+          }
+            // guidelineSearchVal.course_data
+            // guidelineSearchVal.author_data
+            // guidelineSearchVal.coursemode_data
+            // guidelineSearchVal.coursepartner_data
            }else{
             //  this.alert.openAlert('Filter not found',null)
            }
