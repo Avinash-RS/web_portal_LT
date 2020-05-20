9b33ea894b5a5c7c7727f914591a27ab9f49ce38
ankit <b-ankit@lntecc.com>
2020-05-20 12:56:46 +0530

    commit with category checkbox error fixed

--- src/app/learner/pages/view-all-courses/view-all-courses.component.ts
+++ src/app/learner/pages/view-all-courses/view-all-courses.component.ts
@@ -63,13 +63,13 @@ export class ViewAllCoursesComponent implements OnInit {
     if (!this.userDetailes.group_id) {
       this.userDetailes.group_id = '1';
     }
-    this.CommonServices.globalSearch.subscribe((data: any) => {
-      if(data.length > 0) {
-        this.allcourses = data;
-      } else {
-        this.ngOnInit();
-      }
-    })
+    // this.CommonServices.globalSearch.subscribe((data: any) => {
+    //   if(data.length > 0) {
+    //     this.allcourses = data;
+    //   } else {
+    //     this.ngOnInit();
+    //   }
+    // })
     this.loadcategoryandcourses();
   }
 
@@ -102,7 +102,7 @@ export class ViewAllCoursesComponent implements OnInit {
       this. getthreeLevelCat();
     }
 
-    this.learnerservice.getLevelSubCategoryData(this.level1selectedID,this.Lvl2CatId).subscribe((result: any) => {
+    this.learnerservice.getLevelSubCategoryData(this.level1selectedID,this.level2selectedID,this.level3selectedID).subscribe((result: any) => {
       if(result['data']['getLevelSubCategoryData'].success == true){
         this.allLvlCategoryFilterVal = result['data']['getLevelSubCategoryData']['data'];
       }
@@ -193,7 +193,7 @@ export class ViewAllCoursesComponent implements OnInit {
 
 
     var perPage = "10";
-    this.pagenumber = 1;
+    this.pagenumber = 0;
     this.learnerservice.postGuildelineSearchData(this.Lvl1CatId, this.Lvl2CatId, this.Lvl3CatId, this.selectedlang, this.coursemode,
       this.authordetails, this.coursepartners, this.pagenumber, perPage).subscribe((result: any) => {
         this.allcourses = result['data']['getCourseCategorySearch']['data'];
