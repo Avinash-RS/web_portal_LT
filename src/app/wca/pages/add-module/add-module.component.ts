e1f707635ac87aa2f80696cf3a381323173abc78
vijay pagalavan <pagalavanps@gmail.com>
2020-05-22 11:32:14 +0530

    add mod bug fix

--- src/app/wca/pages/add-module/add-module.component.ts
+++ src/app/wca/pages/add-module/add-module.component.ts
@@ -137,22 +137,24 @@ export class AddModuleComponent implements OnInit {
   }
 
   onCreate() {
-    this.spinner.show();
-    this.apiService.createDraft(this.courseDetails).subscribe((res: any) => {
-      if (res.Code == 200) {
-        this.getCourseDetails();
-        const obj = {
-          course_id: this.routedCourseDetails.courseId,
-          is_active: 0
+    if (this.courseDetails.coursetype !== 'SCORM') {
+      this.spinner.show();
+      this.apiService.createDraft(this.courseDetails).subscribe((res: any) => {
+        if (res.Code == 200) {
+          this.getCourseDetails();
+          const obj = {
+            course_id: this.routedCourseDetails.courseId,
+            is_active: 0
+          }
+          this.apiService.updateCourse(obj).subscribe((data: any) => {
+          });
+          this.toast.success('Module updated successfully');
         }
-        this.apiService.updateCourse(obj).subscribe((data: any) => {
-        });
-        this.toast.success('Module updated successfully');
-      }
-      this.spinner.hide();
-    }, err => {
-      this.spinner.hide();
-    })
+        this.spinner.hide();
+      }, err => {
+        this.spinner.hide();
+      })
+    }
   }
 
   addTopic(value, index) {
@@ -161,12 +163,22 @@ export class AddModuleComponent implements OnInit {
   }
 
   navChooseTemp() {
-    this.router.navigate(['/Admin/auth/Wca/choosetemplate'], { queryParams: { addModule: true, viewingModule: this.courseDetails.courseid, courseName: this.courseDetails.coursename, image: this.routedCourseDetails.courseImage } });
-
+    if (this.courseDetails.coursetype !== 'SCORM') {
+      this.router.navigate(['/Admin/auth/Wca/choosetemplate'], { queryParams: { addModule: true, viewingModule: this.courseDetails.courseid, courseName: this.courseDetails.coursename, image: this.routedCourseDetails.courseImage } });
+    }
+    else {
+      this.toast.warning("SCORM course cannot be edited")
+    }
   }
 
   addModuleRepos() {
-    this.router.navigate(['/Admin/auth/Wca/modulerepository'], { queryParams: { viewingModule: this.routedCourseDetails.courseId, courseName: this.routedCourseDetails.courseName, image: this.routedCourseDetails.courseImage, moduleList: this.moduleList } });
+    if (this.courseDetails.coursetype !== 'SCORM') {
+
+      this.router.navigate(['/Admin/auth/Wca/modulerepository'], { queryParams: { viewingModule: this.routedCourseDetails.courseId, courseName: this.routedCourseDetails.courseName, image: this.routedCourseDetails.courseImage, moduleList: this.moduleList } });
+    }
+    else {
+      this.toast.warning("SCORM course cannot be edited")
+    }
   }
 
   crsDetails() {
