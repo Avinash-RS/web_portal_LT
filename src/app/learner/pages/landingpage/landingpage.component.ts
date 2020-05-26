700a25052240f7963aa683619c616c4fb735dfc7
Kanimozhiharish18 <57133205+Kanimozhiharish18@users.noreply.github.com>
2020-05-26 19:25:52 +0530

    landing page course  added

--- src/app/learner/pages/landingpage/landingpage.component.ts
+++ src/app/learner/pages/landingpage/landingpage.component.ts
@@ -6,8 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landingpage.component.scss']
 })
 export class LandingpageComponent implements OnInit {
+  courses: { name: string; description: string; src: string; }[];
+  hoverIdx = -1;
 
-  constructor() { }
+  constructor() {
+    this.courses = [
+      {
+        name: 'Javascript',
+        description:
+          // tslint:disable-next-line:max-line-length
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        src: 'assets/courses/1.jpg',
+      },
+      {
+        name: 'Angular',
+        description:
+          // tslint:disable-next-line:max-line-length
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        src: 'assets/courses/2.jpg',
+      },
+      {
+        name: 'Web development',
+        description:
+          // tslint:disable-next-line:max-line-length
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        src: 'assets/courses/3.jpg',
+      },
+      {
+        name: 'Coding',
+        description:
+          // tslint:disable-next-line:max-line-length
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        src: 'assets/courses/4.jpg',
+      },
+    ];
+
+  }
 
   ngOnInit() {
   }
