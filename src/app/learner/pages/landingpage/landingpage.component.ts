409d1d9918fbdb130278689d6d438cd9e2f83a67
mythreyi <mythreyi.gullapalli@cintanatech.com>
2020-05-27 11:58:30 +0530

    updated

--- src/app/learner/pages/landingpage/landingpage.component.ts
+++ src/app/learner/pages/landingpage/landingpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-landingpage',
@@ -8,6 +9,31 @@ import { Component, OnInit } from '@angular/core';
 export class LandingpageComponent implements OnInit {
   courses: { name: string; description: string; src: string; }[];
   hoverIdx = -1;
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  };
 
   constructor() {
     this.courses = [
