38c9795efa3965ae4abbbc08d56d4af957bb84ba
Kanimozhiharish18 <57133205+Kanimozhiharish18@users.noreply.github.com>
2020-05-27 18:13:56 +0530

    updated card

--- src/app/learner/pages/landingpage/landingpage.component.ts
+++ src/app/learner/pages/landingpage/landingpage.component.ts
@@ -7,8 +7,9 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
   styleUrls: ['./landingpage.component.scss']
 })
 export class LandingpageComponent implements OnInit {
-  courses: { name: string; description: string; src: string; }[];
-  hoverIdx = -1;
+  courses: { name: string; description: string; src: string; rating: number }[];
+  // hoverIdx = -1;
+  breakpoint: number;
 
   customOptions: OwlOptions = {
     loop: false,
@@ -43,6 +44,7 @@ export class LandingpageComponent implements OnInit {
           // tslint:disable-next-line:max-line-length
           'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
         src: 'assets/courses/1.jpg',
+        rating: 5,
       },
       {
         name: 'Angular',
@@ -50,6 +52,7 @@ export class LandingpageComponent implements OnInit {
           // tslint:disable-next-line:max-line-length
           'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
         src: 'assets/courses/2.jpg',
+        rating: 5,
       },
       {
         name: 'Web development',
@@ -57,6 +60,7 @@ export class LandingpageComponent implements OnInit {
           // tslint:disable-next-line:max-line-length
           'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
         src: 'assets/courses/3.jpg',
+        rating: 5,
       },
       {
         name: 'Coding',
@@ -64,6 +68,7 @@ export class LandingpageComponent implements OnInit {
           // tslint:disable-next-line:max-line-length
           'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
         src: 'assets/courses/4.jpg',
+        rating: 5,
       },
       {
         name: 'Javascript',
@@ -71,6 +76,7 @@ export class LandingpageComponent implements OnInit {
           // tslint:disable-next-line:max-line-length
           'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
         src: 'assets/courses/1.jpg',
+        rating: 5,
       },
       {
         name: 'Angular',
@@ -78,13 +84,15 @@ export class LandingpageComponent implements OnInit {
           // tslint:disable-next-line:max-line-length
           'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
         src: 'assets/courses/2.jpg',
+        rating: 5,
       },
       {
-        name: 'Web development',
+        name: 'Web development and Web Design',
         description:
           // tslint:disable-next-line:max-line-length
           'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
         src: 'assets/courses/3.jpg',
+        rating: 5,
       },
       {
         name: 'Coding',
@@ -92,12 +100,34 @@ export class LandingpageComponent implements OnInit {
           // tslint:disable-next-line:max-line-length
           'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
         src: 'assets/courses/4.jpg',
+        rating: 5,
       },
     ];
 
   }
 
   ngOnInit() {
+    if (window.innerWidth <= 600) {
+      this.breakpoint = 1;
+    } else if (window.innerWidth >= 600 && window.innerWidth <= 768) {
+      this.breakpoint = 2;
+    } else if (window.innerWidth >= 768 && window.innerWidth <= 1024) {
+      this.breakpoint = 3;
+    } else {
+      this.breakpoint = 4;
+    }
+  }
+
+  onResize(event) {
+    if (event.target.innerWidth <= 600) {
+      this.breakpoint = 1;
+    } else if (event.target.innerWidth >= 600 && event.target.innerWidth <= 768) {
+      this.breakpoint = 2;
+    } else if (event.target.innerWidth >= 768 && event.target.innerWidth <= 1024) {
+      this.breakpoint = 3;
+    } else {
+      this.breakpoint = 4;
+    }
   }
 
 }
