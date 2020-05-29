2c133b80ee0edd61d240e36380230ddd49f9b2bb
ankit <b-ankit@lntecc.com>
2020-05-29 17:25:54 +0530

    Update core.module.ts

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -88,6 +88,7 @@ const routes: Routes = [
     CapslockDirective,
     StickyHeaderDirective,
     DataTableComponent,
+    VideoPlayerComponent,
     ListViewCourseComponentComponent,
     GuidelineSearchComponent]
 })
