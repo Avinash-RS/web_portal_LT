fe8beb4cee1f173fbb11dfd74940106991bfe65a
Harish Arulkumar <harisharul@lntecc.com>
2020-05-29 09:02:36 +0000

    Added environment.qa.ts

---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://20.44.41.200:3000/',
+  scormUrl: 'http://20.44.36.116:8089/',
+  apiUrlImg: 'http://20.44.41.200:3000/',
+  createCourseApi: 'http://20.44.41.55:3002/',
+  wcaapiurl: 'http://20.44.34.169:9001/',
+};
