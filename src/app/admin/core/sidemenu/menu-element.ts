38cd56fc958c07fa89fee5c0ba4dcc8fc321dbb7
mythreyi <mythreyi.gullapalli@cintanatech.com>
2020-06-04 22:02:22 +0530

    updated

--- src/app/admin/core/sidemenu/menu-element.ts
+++ src/app/admin/core/sidemenu/menu-element.ts
@@ -1,8 +1,94 @@
+// export const menus = [
+//     {
+//         name: 'Dashboard',
+//         icon: 'dashboard',
+//         link: '/Admin/auth/dashboard',
+//         open: false,
+//         chip: false,
+
+//     },
+//     {
+//         name: 'User Management',
+//         icon: 'widgets',
+//         link: false,
+//         open: false,
+//         sub: [
+//             {
+//                 name: 'Users',
+//                 link: '/Admin/auth/userManagement',
+//                 icon: 'widgets',
+//                 chip: false,
+//                 open: true,
+//             },
+//             {
+//                 name: 'User Groups',
+//                 link: '/Admin/auth/usergroup',
+//                 icon: 'list',
+//                 chip: false,
+//                 open: true,
+//             },
+//             {
+//                 name: 'Course Enrollment',
+//                 icon: 'border_color',
+//                 link: '/Admin/auth/enrollment',
+//                 open: false,
+//                 chip: false,
+//             },
+//             {
+//                 name: 'Reports',
+//                 link: '/Admin/auth/reports',
+//                 icon: 'notes',
+//                 chip: false,
+//                 open: true,
+//             },
+//         ]
+//     },
+//     {
+//         name: 'Course Management',
+//         icon: 'list',
+//         link: '/Admin/auth/Wca',
+//         chip: false,
+//         open: true,
+
+//     },
+
+//     {
+//         name: 'Catalogue Management',
+//         icon: 'business',
+//         link: false,
+//         open: false,
+//         sub: [
+//             {
+//                 name: 'Categories',
+//                 link: '/Admin/auth/catagory',
+//                 icon: 'view_quilt',
+//                 chip: false,
+//                 open: true,
+//             },
+//             {
+//                 name: 'Catalogues',
+//                 link: '/Admin/auth/catalogue',
+//                 icon: 'assignment',
+//                 chip: false,
+//                 open: true,
+//             }
+//         ]
+//     },
+//     {
+//         name: 'Audit Log',
+//         link: '/Admin/auth/auditlog',
+//         icon: 'widgets',
+//         chip: false,
+//         open: true,
+//     },
+// ];
+
+
 export const menus = [
     {
         name: 'Dashboard',
         icon: 'dashboard',
-        link: '/Admin/auth/dashboard',
+        link: 'dashboard',
         open: false,
         chip: false,
 
@@ -15,14 +101,14 @@ export const menus = [
         sub: [
             {
                 name: 'Users',
-                link: '/Admin/auth/userManagement',
+                link: 'userManagement',
                 icon: 'widgets',
                 chip: false,
                 open: true,
             },
             {
                 name: 'User Groups',
-                link: '/Admin/auth/usergroup',
+                link: 'usergroup',
                 icon: 'list',
                 chip: false,
                 open: true,
@@ -30,13 +116,13 @@ export const menus = [
             {
                 name: 'Course Enrollment',
                 icon: 'border_color',
-                link: '/Admin/auth/enrollment',
+                link: 'enrollment',
                 open: false,
                 chip: false,
             },
             {
                 name: 'Reports',
-                link: '/Admin/auth/reports',
+                link: 'reports',
                 icon: 'notes',
                 chip: false,
                 open: true,
@@ -46,7 +132,7 @@ export const menus = [
     {
         name: 'Course Management',
         icon: 'list',
-        link: '/Admin/auth/Wca',
+        link: 'Wca',
         chip: false,
         open: true,
 
@@ -60,14 +146,14 @@ export const menus = [
         sub: [
             {
                 name: 'Categories',
-                link: '/Admin/auth/catagory',
+                link: 'catagory',
                 icon: 'view_quilt',
                 chip: false,
                 open: true,
             },
             {
                 name: 'Catalogues',
-                link: '/Admin/auth/catalogue',
+                link: 'catalogue',
                 icon: 'assignment',
                 chip: false,
                 open: true,
@@ -76,7 +162,7 @@ export const menus = [
     },
     {
         name: 'Audit Log',
-        link: '/Admin/auth/auditlog',
+        link: 'auditlog',
         icon: 'widgets',
         chip: false,
         open: true,
