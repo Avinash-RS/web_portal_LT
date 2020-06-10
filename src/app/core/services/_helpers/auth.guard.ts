826ef68ae553ac07318954d4b0e29a2907a6e148
ankit <b-ankit@lntecc.com>
2020-06-10 18:24:12 +0530

    commit with logout error

--- src/app/core/services/_helpers/auth.guard.ts
+++ src/app/core/services/_helpers/auth.guard.ts
@@ -19,7 +19,7 @@ export class AuthGuard implements CanActivate {
     const userDetailes = JSON.parse(localStorage.getItem('UserDetails')) || null;
     const adminDetails = JSON.parse(localStorage.getItem('adminDetails')) || null;
     const role = localStorage.getItem('role') || null;
-      // console.log('role-----',role)
+    // console.log('role-----',role)
     // for learner ------> 1
     // debugger
     if (userDetailes != null && role === 'learner' &&
@@ -29,28 +29,32 @@ export class AuthGuard implements CanActivate {
         state.url !== '/Admin/auth/Wca/addcourse' && state.url !== '/Admin/auth/Wca/wca' && state.url !== '/Admin/auth/Wca/viewmodule' &&
         state.url !== '/Admin/auth/Wca/createmodule' && state.url !== '/Admin/auth/Wca/addmodule' &&
         state.url !== '/Admin/auth/Wca/choosetemplate' && state.url !== '/Admin/auth/Wca/previewcourse' &&
-        state.url !== '/Admin/auth/Wca/rf' &&  state.url !== '/Admin/auth/Wca/addtemplate' &&
-        state.url !== '/Admin/auth/Wca/addtopic' &&        state.url !== '/Admin/auth/Wca/ct' &&
+        state.url !== '/Admin/auth/Wca/rf' && state.url !== '/Admin/auth/Wca/addtemplate' &&
+        state.url !== '/Admin/auth/Wca/addtopic' && state.url !== '/Admin/auth/Wca/ct' &&
         state.url !== '/Admin/auth/Wca/addfile' &&
         state.url !== '/Admin/auth/Wca/mycourse')) {
       // userdetail is present // authenticated user
       // url should not start from admin - can be /Larner or anything
       // if profile updated and trying to go login/reg
-      if ((state.url === '/Learner/login' || state.url === '/Admin/login' || state.url === '/Learner/register')) {
-        this.router.navigate(['/Learner']);
+      // if ((state.url === '/Learner/login' || state.url === '/Admin/login' || state.url === '/Learner/register')) {
+      //   this.router.navigate(['/Learner']);
+      //   return false;
+      if ((state.url === '/Learner/login' || state.url === '/Admin/login' || state.url === '/Learner/register' ||
+        state.url === '/' || state.url === '/Learner')) {
+        this.router.navigate(['/Learner/profile']);
         return false;
       } else if (!userDetailes.is_profile_updated) {
         // if profile not updated and trying to access other screens, redirect to profile
         if (state.url !== '/Learner/profile') {
           this.router.navigate(['/Learner/profile']);
-          this.alert.openAlert('Your profile is incomplete !', 'Please provide data for all mandatory fields');
+          this.alert.openAlert('Your profile is incomplete !', 'Please fill all mandatory details');
           return false;
         } else {// if url is profile or anything
           return true;
         }
       } else {
         return true;
-           }
+      }
       // end of url navigations for logged in learner ------> 1
     } else if ((userDetailes == null || adminDetails == null) && role == null) { // user detail is not present in local storage
       if (state.url === '/Learner' || state.url === '/Learner/login' || state.url === '/Admin/login'
@@ -71,7 +75,7 @@ export class AuthGuard implements CanActivate {
         || state.url === '/Admin/auth/Wca/addtopic' || state.url === '/Admin/auth/Wca/rf'
         || state.url === '/Admin/auth/Wca/addfile' || state.url === '/Admin/auth/Wca/addmodule'
         || state.url === '/Admin/auth/Wca/mycourse' || state.url === '/Admin/auth/Wca/previewcourse'
-        || state.url === '/Admin/auth/Wca/ct'  || state.url === '/Learner/login') {
+        || state.url === '/Admin/auth/Wca/ct' || state.url === '/Learner/login') {
         return true;
       }
     } else if (role === 'admin' && !adminDetails) {
