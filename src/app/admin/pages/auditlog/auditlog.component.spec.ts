692d91e8a93405ed26ca7803c78a9d80f9df08c1
Mythreyi <Mythreyi@intecc.com>
2020-06-11 20:34:45 +0530

    auditlog

--- src/app/admin/pages/auditlog/auditlog.component.spec.ts
+++ src/app/admin/pages/auditlog/auditlog.component.spec.ts
@@ -1,18 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuditlogComponent } from './auditlog.component';
-import { MatDatepickerModule, MatFormFieldModule, MatTableModule, MatNativeDateModule } from '@angular/material';
+import { MatDatepickerModule, MatFormFieldModule, MatTableModule, MatNativeDateModule, MatDialogModule, MatDialogRef, MatDialogTitle, MAT_DIALOG_DATA } from '@angular/material';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ApolloModule } from 'apollo-angular';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
+import { AdminServicesService } from '@admin/services/admin-services.service';
 describe('AuditlogComponent', () => {
   let component: AuditlogComponent;
   let fixture: ComponentFixture<AuditlogComponent>;
-
+  const dialogMock = {
+    closeAll: () => { }
+    };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports:[
@@ -25,13 +27,26 @@ describe('AuditlogComponent', () => {
         HttpClientModule,
         RouterModule.forRoot([]),
         MatNativeDateModule,
+        MatDialogModule,
         // MatFormFieldModule,
       ],
       schemas: [
         CUSTOM_ELEMENTS_SCHEMA,
         NO_ERRORS_SCHEMA
       ],
-      declarations: [ AuditlogComponent ]
+      declarations: [ AuditlogComponent ],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: {}
+        },
+        { provide: MatDialogRef, useValue:  dialogMock },
+        { provide: MatDialogTitle , useValue: [] },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+    
+        
+        // ...
+      ],
     })
     .compileComponents();
   }));
@@ -45,4 +60,22 @@ describe('AuditlogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('closedialog()', () => {
+    let spy = spyOn(component.dialog, 'closeAll').and.callThrough();
+    component.closedialogbox();
+    expect(spy).toHaveBeenCalled();    
+  });
+
+  it('destroy', () => {
+    let spy = spyOn(component.dialog, 'closeAll').and.callThrough();
+    component.ngOnDestroy();
+    expect(spy).toHaveBeenCalled();    
+  });
+
+  // it('openviewdialog', () => {
+
+  //   component. openviewdialog(data, templateRef)
+  // });
+
 });
