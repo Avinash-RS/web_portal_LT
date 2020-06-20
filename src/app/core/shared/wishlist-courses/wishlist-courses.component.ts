eeb7ec523c12cfd9eed55ce0999f7350da1de4ad
KANIMOZHI-HARISH <kanimozhi-harish@lntecc.com>
2020-06-20 19:38:23 +0530

    course, wishlist changes

--- src/app/core/shared/wishlist-courses/wishlist-courses.component.ts
+++ src/app/core/shared/wishlist-courses/wishlist-courses.component.ts
@@ -15,39 +15,40 @@ export class WishlistCoursesComponent implements OnInit {
   @Input('canNavigate') canNavigate: boolean;
   @Input('showStatus') showStatus: boolean;
   breakpoint: number;
+  pagenumber = 0;
 
   constructor(public service: CommonServicesService, private gs: GlobalServiceService, ) { }
 
-  ngOnInit() {
+  ngOnInit() { 
     if (this.gs.checkLogout()) {
       this.viewWishlist();
       this.gs.callWishlist.subscribe(message =>
         this.viewWishlist()
-      )
+      );
     }
-    if (window.innerWidth <= 480)
-      this.breakpoint = 1;
-    else if (window.innerWidth >= 480 && window.innerWidth <= 768)
-      this.breakpoint = 2;
-    else if (window.innerWidth >= 768 && window.innerWidth <= 992) this.breakpoint = 3;
-    // else if (window.innerWidth >= 992 && window.innerWidth <= 1200)
+    // if (window.innerWidth <= 480)
+    //   this.breakpoint = 1;
+    // else if (window.innerWidth >= 480 && window.innerWidth <= 768)
+    //   this.breakpoint = 2;
+    // else if (window.innerWidth >= 768 && window.innerWidth <= 992) this.breakpoint = 3;
+    // // else if (window.innerWidth >= 992 && window.innerWidth <= 1200)
+    // //   this.breakpoint = 4;
+    // else
     //   this.breakpoint = 4;
-    else
-      this.breakpoint = 4;
   }
 
-  onResize(event) {
-    if (event.target.innerWidth <= 480)
-      this.breakpoint = 1;
-    else if (event.target.innerWidth >= 480 && event.target.innerWidth <= 768)
-      this.breakpoint = 2;
-    else if (event.target.innerWidth >= 768 && event.target.innerWidth <= 992)
-      this.breakpoint = 3;
-    // else if (event.target.innerWidth >= 992 && event.target.innerWidth <= 1200)
-    //   this.breakpoint = 4;
-    else
-      this.breakpoint = 4;
-  }
+  // onResize(event) {
+  //   if (event.target.innerWidth <= 480)
+  //     this.breakpoint = 1;
+  //   else if (event.target.innerWidth >= 480 && event.target.innerWidth <= 768)
+  //     this.breakpoint = 2;
+  //   else if (event.target.innerWidth >= 768 && event.target.innerWidth <= 992)
+  //     this.breakpoint = 3;
+  //   // else if (event.target.innerWidth >= 992 && event.target.innerWidth <= 1200)
+  //   //   this.breakpoint = 4;
+  //   else
+  //     this.breakpoint = 4;
+  // }
 
   viewWishlist() {
     const userdetail = this.gs.checkLogout();
@@ -57,4 +58,17 @@ export class WishlistCoursesComponent implements OnInit {
       }
     });
   }
+
+  nextGetWishList() {
+    this.pagenumber = this.pagenumber + 1;
+    console.log(this.pagenumber);
+    // this.service.viewWishlist(this.pagenumber).subscribe((result: any) => {
+    //   this.viewWishlist.push(...result?.data?.viewWishlist?.message);
+    //   };
+    }
+    // this.adminservice.getAllCatalogue(this.pagenumber).subscribe((result: any) => {
+    //   this.catalogueList.push(...result?.data?.getallcatalogue?.message);
+    // });
+  // }
+
 }
