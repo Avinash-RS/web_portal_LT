978e07358d14b891c6394bf14a0bf4b44911a0db
ankit <b-ankit@lntecc.com>
2020-06-23 14:36:00 +0530

    commit with course details page

--- src/app/learner/pages/coursedetails/coursedetails.component.ts
+++ src/app/learner/pages/coursedetails/coursedetails.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CommonServicesService } from '@core/services/common-services.service';
 import { GlobalServiceService } from '@core/services/handlers/global-service.service';
-import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+import { environment } from '../../../../environments/environment';
 import { AlertServiceService } from '@core/services/handlers/alert-service.service';
 import { LearnerServicesService } from '@learner/services/learner-services.service';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -30,7 +30,7 @@ export class CoursedetailsComponent implements OnInit {
       }
     },
     nav: true
-  }
+  };
 
   customOptions: any = {
     loop: true,
@@ -58,110 +58,133 @@ export class CoursedetailsComponent implements OnInit {
   };
   wishlist: any = [];
   syllabus: {}[];
-  open: boolean = false;
+  open = false;
   userDetail: any;
   showShortDesciption = true;
   clicked: any = 'media';
   content: any;
   modulength: any;
-  urlSafe: any;
   isCollapsed: any;
-  constructor(private router: ActivatedRoute, public Lservice: LearnerServicesService, public service: CommonServicesService, private gs: GlobalServiceService,
-    public route: Router, private loader: Ng4LoadingSpinnerService, private alert: AlertServiceService,
-    public sanitizer: DomSanitizer) {
-      
-    var detail = (this.route.getCurrentNavigation() && this.route.getCurrentNavigation().extras &&
+  panelOpenState = false;
+  showFiller = false;
+  courseTime: any;
+  url: string;
+  urlSafe: SafeResourceUrl;
+  userid: any;
+  courseid: any;
+  contentid: string;
+  constructor(private router: ActivatedRoute, public Lservice: LearnerServicesService,
+              public service: CommonServicesService, private gs: GlobalServiceService,
+              public route: Router, private alert: AlertServiceService,
+              public sanitizer: DomSanitizer) {
+
+    const detail = (this.route.getCurrentNavigation() && this.route.getCurrentNavigation().extras &&
       this.route.getCurrentNavigation().extras.state && this.route.getCurrentNavigation().extras.state.detail);
-      
-    this.service.viewCurseByID(detail && detail.id || '1').subscribe((viewCourse: any) => {
-      // this.loadingCourse = true;
-      if (viewCourse.data.viewcourse && viewCourse.data.viewcourse.success) {
-        this.course = viewCourse.data.viewcourse.message;
-        // this.loadingCourse = false;
-        this.course.wishlisted = detail.wishlist || false;
-        this.course.wishlist_id = detail.wishlist_id || null;
-        this.course.enrollment_status = detail.enrollment_status;
-        console.log(this.course)
-      } else{
+    if (this.gs.checkLogout()) {
+      this.courseid = detail.id;
+      this.userDetail = this.gs.checkLogout();
+      this.service.viewCurseByID(detail && detail.id || '1', this.userDetail.user_id)
+        .subscribe((viewCourse: any) => {
+          if (viewCourse.data.viewcourse && viewCourse.data.viewcourse.success) {
+            this.course = viewCourse.data.viewcourse.message;
+            if (this.course.topicData && this.course.topicData.length) {
+              // this.topicData = [];
+              this.course.topicData.forEach(element => {
+                const subArr = [];
+                element.moduleData.forEach(element1 => {
+                  subArr.push(element1.moduledetails);
+                });
+                const obj = {
+                  modulename: element.moduleData[0].modulename,
+                  moduledetails: subArr
+                };
+                // this.topicData.push(obj);
+              });
+            }
+            // this.course.topicData = this.topicData;
 
-      }
-      // this.loadingCourse = false;
+            this.course.wishlisted = detail.wishlist || false;
+            this.course.wishlist_id = detail.wishlist_id || null;
+            this.course.enrollment_status = detail.enrollment_status;
+          }
+        });
+      this.passCourseId();
+      this.contentid = 'dfdfd';
+      this.url = environment.scormUrl + 'scormPlayer.html?contentID=' + this.contentid + '&user_id=' +
+          this.userid + '&course_id=' + this.courseid;
+
+    }
+    this.Lservice.getModuleData(detail.id).subscribe((data: any) => {
+      this.content = data.data.getmoduleData.data[0];
+      this.modulength = this.content.coursedetails.length;
+      this.courseTime = this.content.coursetime;
     });
-    this.Lservice.getModuleData(detail.id).subscribe(data => {
-      this.content = data.data['getmoduleData']['data'][0];
-      this.modulength = this.content['coursedetails'].length;
-      console.log(this.content, 'course details')
-    })
   }
-
   clickedT(i) {
-    this.clicked = i
+    this.clicked = i;
   }
 
   alterDescriptionText() {
-    this.showShortDesciption = !this.showShortDesciption
+    this.showShortDesciption = !this.showShortDesciption;
   }
   ngOnInit() {
-    // this.service.list_content().subscribe((list_content: any) => {
-    //   if (list_content.data.list_content.success) {
-    //     this.syllabus = list_content.data.list_content.data
-    //   }
-    // });
-
     if (this.gs.checkLogout()) {
-      this.userDetail = this.gs.checkLogout()
+      this.userDetail = this.gs.checkLogout();
     }
   }
-
-  scroll(el: HTMLElement) {
-    el.scrollTop = 0;
-    el.scrollIntoView({ behavior: 'smooth' });
-  }
-
-  playCourse(i) {
-    this.route.navigate(["/Learner/scorm", { id: i }]);
-    this.service.syllabus_of_particular_scorm('FSL ').subscribe((viewCourse: any) => {
+  passCourseId() {
+    this.service.geturl(this.courseid).subscribe((data: any) => {
     });
   }
-
-  selectWishlist(course) {
-    this.loader.show()
-    if (this.gs.checkLogout()) {
-      if (this.course.wishlisted == false) {
-        this.service.addWishlist(course.course_id, this.userDetail._id).subscribe((addWishlist: any) => {
-          if (addWishlist.data.add_to_wishlist && addWishlist.data.add_to_wishlist.success) {
-            this.course.wishlisted = !this.course.wishlisted;
-            this.course.wishlist_id = addWishlist.data.add_to_wishlist.wishlist_id;
-            this.gs.canCallWishlist(true)
-            this.loader.hide()
-          }
-        });
-      } else {
-        this.service.removeWishlist(course.wishlist_id).subscribe((addWishlist: any) => {
-          if (addWishlist.data.delete_wishlist && addWishlist.data.delete_wishlist.success) {
-            this.course.wishlisted = !this.course.wishlisted;
-            course.wishlist_id = null;
-            this.gs.canCallWishlist(true)
-            this.loader.hide()
-          }
-        });
-      }
+    scroll(el: HTMLElement) {
+      el.scrollTop = 0;
+      el.scrollIntoView({ behavior: 'smooth' });
     }
-  }
 
-  enrollCourse() {
-    this.service.enrollcourse(this.userDetail.user_id, this.userDetail.group_id[0], this.course.course_id).subscribe((enrollCourse: any) => {
-      if (enrollCourse.data) {
-        if (enrollCourse.data.enrollcourse.success) {
-          Swal.fire("User enrolled successfully for the course")
+    playCourse(i) {
+      this.route.navigate(['/Learner/scorm', { id: i }]);
+      this.service.syllabus_of_particular_scorm('FSL ').subscribe((viewCourse: any) => {
+      });
+    }
+
+    selectWishlist(course) {
+      // this.loader.show();
+      if (this.gs.checkLogout()) {
+        if (this.course.wishlisted === false) {
+          this.service.addWishlist(course.course_id, this.userDetail._id).subscribe((addWishlist: any) => {
+            if (addWishlist.data.add_to_wishlist && addWishlist.data.add_to_wishlist.success) {
+              this.course.wishlisted = !this.course.wishlisted;
+              this.course.wishlist_id = addWishlist.data.add_to_wishlist.wishlist_id;
+              this.gs.canCallWishlist(true);
+              // this.loader.hide();
+            }
+          });
         } else {
-          Swal.fire(enrollCourse.data.enrollcourse.message)
+          this.service.removeWishlist(course.wishlist_id).subscribe((addWishlist: any) => {
+            if (addWishlist.data.delete_wishlist && addWishlist.data.delete_wishlist.success) {
+              this.course.wishlisted = !this.course.wishlisted;
+              course.wishlist_id = null;
+              this.gs.canCallWishlist(true);
+              // this.loader.hide();
+            }
+          });
         }
       }
-      else {
-        Swal.fire("Please try again later")
-      }
-    });
-  }
+    }
 
-}
+    enrollCourse() {
+      this.service.enrollcourse(this.userDetail.user_id, this.userDetail.group_id[0], this.course.course_id)
+        .subscribe((enrollCourse: any) => {
+          if (enrollCourse.data) {
+            if (enrollCourse.data.enrollcourse.success) {
+              Swal.fire('User enrolled successfully for the course');
+            } else {
+              Swal.fire(enrollCourse.data.enrollcourse.message);
+            }
+          } else {
+            Swal.fire('Please try again later');
+          }
+        });
+    }
+
+  }
--- src/app/learner/pages/scormplayer/scormplayer.component.ts
+++ src/app/learner/pages/scormplayer/scormplayer.component.ts
@@ -6,7 +6,7 @@ import { environment } from '../../../../environments/environment';
 import { CommonServicesService } from '@core/services/common-services.service';
 import { AlertServiceService } from '@core/services/handlers/alert-service.service';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { MatDialog ,MatDialogConfig} from '@angular/material';
+import { MatDialog , MatDialogConfig} from '@angular/material';
 
 @Component({
   selector: 'app-scormplayer',
@@ -20,9 +20,9 @@ export class ScormplayerComponent implements OnInit {
   modules: any[];
   authorDetails: any[];
   name = 'Set iframe source';
-  url: string
+  url: string;
   urlSafe: SafeResourceUrl;
-  user_id: any
+  user_id: any;
   breakpoint: number;
   course_id: any;
   courseDeatils: any;
@@ -32,79 +32,81 @@ export class ScormplayerComponent implements OnInit {
   question_id: any = [];
   jsonData: any;
   allFeedbackQue: any;
-  show: boolean = false
-  constructor(private dialog: MatDialog, public sanitizer: DomSanitizer, public spinner: NgxSpinnerService, public activatedRoute: ActivatedRoute, private alert: AlertServiceService,
-    public service: LearnerServicesService, public route: Router, public commonService: CommonServicesService, ) {
-    var detail = (this.route.getCurrentNavigation() && this.route.getCurrentNavigation().extras &&
+  show = false;
+  constructor(private dialog: MatDialog, public sanitizer: DomSanitizer,
+              public spinner: NgxSpinnerService, public activatedRoute: ActivatedRoute, private alert: AlertServiceService,
+              public service: LearnerServicesService, public route: Router, public commonService: CommonServicesService, ) {
+    const detail = (this.route.getCurrentNavigation() && this.route.getCurrentNavigation().extras &&
       this.route.getCurrentNavigation().extras.state && this.route.getCurrentNavigation().extras.state.detail);
 
 
     this.contentid = detail.id;
     this.user_id = detail.user;
-    this.course_id = detail.course_id
-    localStorage.setItem('scorm_user_id', this.user_id)
-    localStorage.setItem('course_id', this.course_id)
+    this.course_id = detail.course_id;
+    localStorage.setItem('scorm_user_id', this.user_id);
+    localStorage.setItem('course_id', this.course_id);
     this.spinner.show();
-    this.commonService.viewCurseByID(this.course_id).subscribe(data => {
-      if (data.data['viewcourse'].success == true) {
-        this.courseDeatils = data.data['viewcourse']['message'];
+    this.commonService.viewCurseByID(this.course_id, this.user_id).subscribe((data:any) => {
+      if (data.data.viewcourse.success === true) {
+        this.courseDeatils = data.data.viewcourse.message;
         this.spinner.hide();
       } else {
         this.spinner.hide();
-        this.alert.openAlert(data.data['viewcourse']['error_msg'], null)
+        this.alert.openAlert(data.data.viewcourse.error_msg, null);
       }
 
-    })
-    if (detail.feed_back == 1) {
+    });
+    if (detail.feed_back === 1) {
       this.show = true;
-    }else{
-      this.show=false;
+    } else {
+      this.show = false;
     }
   }
 
   ngOnInit() {
     this.breakpoint = (window.innerWidth <= 400) ? 1 : 2;
     this.passCourseId();
-    this.contentid = 'dfdfd'
-    this.url = environment.scormUrl + 'scormPlayer.html?contentID=' + this.contentid + '&user_id=' + this.user_id + '&course_id=' + this.course_id
+    this.contentid = 'dfdfd';
+    this.url = environment.scormUrl + 'scormPlayer.html?contentID=' + this.contentid + '&user_id=' +
+    this.user_id + '&course_id=' + this.course_id;
     this.getModuleData();
   }
   getcontent() {
     this.service.list_content().subscribe(data => {
-    })
+    });
   }
   passCourseId() {
     this.commonService.geturl(this.course_id).subscribe(data => {
-    })
+    });
   }
 
   getModuleData() {
-    this.service.getModuleData(this.course_id).subscribe(data => {
-      if (data.data['getmoduleData']['success'] === 'true') {
-        this.content = data.data['getmoduleData']['data'][0];
-        this.urlSafe= this.sanitizer.bypassSecurityTrustResourceUrl(this.content.url);
-        //this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl('../../../../assets/scormContent' + this.content.url);
-        this.modulength = this.content['coursedetails'].length;
+    this.service.getModuleData(this.course_id).subscribe((data: any) => {
+      if (data.data.getmoduleData.success === 'true') {
+        this.content = data.data.getmoduleData.data[0];
+        this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.content.url);
+        // this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl('../../../../assets/scormContent' + this.content.url);
+        this.modulength = this.content.coursedetails.length;
         this.content.coursedetails.forEach(moduledetails => {
           moduledetails.moduledetails.forEach(element => {
             this.countofdoc = element.resourse.count;
-            return true
+            return true;
           });
         });
       }
-    })
+    });
   }
 
   downloadAll(urls) {
-    var arr: any = [];
+    const arr: any = [];
     urls.forEach(element => {
       arr.push(element.path);
     });
-    var link = document.createElement('a');
+    const link = document.createElement('a');
     link.target = '_blank';
     link.style.display = 'none';
     document.body.appendChild(link);
-    for (var i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
       link.href = arr[i];
       link.click();
     }
@@ -112,10 +114,10 @@ export class ScormplayerComponent implements OnInit {
   }
   moresection(vale, modelenght) {
     this.modulength = modelenght - 5;
-    if (vale == true) {
-      this.isCollapsed = false
+    if (vale === true) {
+      this.isCollapsed = false;
     } else {
-      this.isCollapsed = true
+      this.isCollapsed = true;
     }
   }
 
@@ -124,26 +126,28 @@ export class ScormplayerComponent implements OnInit {
 
   }
   getFeedbackQue() {
-    this.service.getFeedbackQuestion().subscribe(data => {
-      //console.log(data.data['getFeedbackQuestion']['success'])
-      if (data.data['getFeedbackQuestion']['success'] === true) {
-        ///console.log(data.data['getFeedbackQuestion'])
-        this.allFeedbackQue = data.data['getFeedbackQuestion']['data']
+    this.service.getFeedbackQuestion().subscribe((data: any) => {
+      // console.log(data.data['getFeedbackQuestion']['success'])
+      if (data.data.getFeedbackQuestion.success === true) {
+        /// console.log(data.data['getFeedbackQuestion'])
+        this.allFeedbackQue = data.data.getFeedbackQuestion.data;
       }
-    })
+    });
   }
   submitFeedback(are, are1, selectOption) {
-    var question_ans: any = [];
-    question_ans.push({ question: 'What do you like about the module ?', answer: are }, { question: 'What could be improved ?', answer: are1 }, { question: 'Would you recommend this to a friend ?', answer: selectOption })
-    this.question_id.question_ans = question_ans
-    this.question_id.user_id = 'hghghjg'
-    this.service.InsertCourseFeedback(this.question_id).subscribe(data => {
-      if (data.data['InsertCourseFeedback']['success'] == true) {
+    const question_ans: any = [];
+    question_ans.push({ question: 'What do you like about the module ?', answer: are },
+     { question: 'What could be improved ?', answer: are1 },
+     { question: 'Would you recommend this to a friend ?', answer: selectOption });
+    this.question_id.question_ans = question_ans;
+    this.question_id.user_id = 'hghghjg';
+    this.service.InsertCourseFeedback(this.question_id).subscribe((data: any) => {
+      if (data.data.InsertCourseFeedback.success === true) {
         this.show = false;
-        console.log('success')
+        console.log('success');
       } else {
-        console.log('fail')
+        console.log('fail');
       }
-    })
+    });
   }
 }
