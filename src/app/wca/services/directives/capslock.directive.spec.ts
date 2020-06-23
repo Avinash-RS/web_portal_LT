55e17d2f3bfa8811337e5646aa32ace3abe59793
Prasanth Shanmugam <prasanth.s@cintana.com>
2020-06-23 22:20:50 +0530

    removing duplicate codes

--- src/app/wca/services/directives/capslock.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CapslockDirective } from './capslock.directive';
-
-describe('CapslockDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CapslockDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/wca/services/directives/drag-drop-image.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DragDropImageDirective } from './drag-drop-image.directive';
-
-describe('DragDropImageDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DragDropImageDirective();
-    expect(directive).toBeTruthy();
-  });
-});
