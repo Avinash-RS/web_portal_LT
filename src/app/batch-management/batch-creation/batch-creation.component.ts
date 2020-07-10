70a299642950a35ceaa49c3cac3a4dffea131079
vijay pagalavan <pagalavanps@gmail.com>
2020-07-10 14:34:20 +0530

    batch creation implementation

--- src/app/batch-management/batch-creation/batch-creation.component.ts
+++ src/app/batch-management/batch-creation/batch-creation.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { WcaService } from '@wca/services/wca.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'batch-creation',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BatchCreationComponent implements OnInit {
 
-  constructor() { }
+  step = 4;
+  instructors = [];
+  branchDetails = {
+    batchname: '',
+    batchdescription: '',
+    batchstartdate: '',
+    batchenddate: '',
+    user_details: [],
+    course_details: [],
+    instructur_details: []
+  };
+  constructor(
+    private wcaService: WcaService,
+    public toast: ToastrService) {
+  }
 
   ngOnInit() {
+
+    this.getInsructors();
+  }
+
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  getInsructors() {
+    this.wcaService.getAllInstructors().subscribe((data: any) => {
+      this.instructors = data.Result;
+    });
+  }
+
+  instrucSelection(e) {
+
+    
+
+  }
+
+  createBatch() {
+    if (this.branchDetails.batchname && this.branchDetails.batchname.length > 0) {
+      this.toast.warning('Batch name is mandatory');
+      
+    }
   }
 
 }
