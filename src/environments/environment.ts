9def6fd3cc0478cb77adc4f5fcf6e50567b1a3dc
Harish Arulkumar <harisharul@lntecc.com>
2020-07-27 07:16:18 +0000

    Updated environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://13.71.123.238:3000/',
-  apiUrl: 'http://20.44.41.200:3000/',
+  apiUrl: 'http://facade.southindia.cloudapp.azure.com:3000/',
   // apiUrl: 'http://20.44.42.101:3000/',
   scormUrl: 'http://20.44.36.116:8089/',
-  apiUrlImg: 'http://20.44.41.200:3000/',
+  apiUrlImg: 'http://facade.southindia.cloudapp.azure.com:3000/',
   createCourseApi: 'http://20.44.41.55:3002/',
   wcaapiurl: 'http://20.44.34.169:9001/',
   
