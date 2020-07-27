166afb42307edd36420cfe39f8c53efa912d1e6c
Harish Arulkumar <harisharul@lntecc.com>
2020-07-27 07:18:05 +0000

    Updated environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,18 +4,12 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://13.71.123.238:3000/',
   apiUrl: 'http://facade.southindia.cloudapp.azure.com:3000/',
-  // apiUrl: 'http://20.44.42.101:3000/',
   scormUrl: 'http://20.44.36.116:8089/',
   apiUrlImg: 'http://facade.southindia.cloudapp.azure.com:3000/',
   createCourseApi: 'http://20.44.41.55:3002/',
   wcaapiurl: 'http://20.44.34.169:9001/',
-  
-  // apiUrl: 'http://13.71.123.238:3000/',
-  // scormUrl:'http://20.44.41.200:8080/',
-  // apiUrlImg:'http://20.44.41.200:3001/',
-  // wcaapiurl:'http://20.44.41.200:9001/',
+
   domain : 'LXP'
 };
 
