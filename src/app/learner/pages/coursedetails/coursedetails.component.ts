49bd4ada470097c4c10529c5c9e3ed1db63f6a18
Ramya Thothathiri <ramyathothathiri@lntecc.com>
2020-08-21 20:40:55 +0530

    Upload issue fixed

--- src/app/learner/pages/coursedetails/coursedetails.component.ts
+++ src/app/learner/pages/coursedetails/coursedetails.component.ts
@@ -277,21 +277,17 @@ export class CoursedetailsComponent implements OnInit {
                 const endDate = new Date(date2);
                 fileData.assignmentStartDate = moment(startDate).format('DD-MM-YYYY HH:MM');
                 fileData.assignmentEndDate = moment(endDate).format('DD-MM-YYYY HH:MM');
-                console.log(fileData.assignmentStartDate);
-                console.log(fileData.assignmentEndDate);
-
                 if (moment().format('DD-MM-YYYY HH:MM') >= fileData.assignmentStartDate) {
                   fileData.enableView = true;
                 } else {
                   fileData.enableView = false;
                 }
-
                 if (moment().format('DD-MM-YYYY HH:MM') >= fileData.assignmentStartDate &&
                   moment().format('DD-MM-YYYY HH:MM') <= this.courseEndDate) {
-                  this.assignmentContent.enableUpload = true;
+                    fileData.enableUpload = true;
                 } else if (moment().format('DD-MM-YYYY HH:MM') < fileData.assignmentStartDate ||
                   moment().format('DD-MM-YYYY HH:MM') > this.courseEndDate) {
-                  this.assignmentContent.enableUpload = false;
+                    fileData.enableUpload = false;
                 }
               }
             });
