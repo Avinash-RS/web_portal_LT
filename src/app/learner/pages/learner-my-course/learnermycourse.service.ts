15de4916bbd891d664c0020a2f864e6582c841b2
Ramya Thothathiri <ramyathothathiri@lntecc.com>
2020-09-29 16:10:15 +0530

    issue fixed while login

--- src/app/learner/pages/learner-my-course/learnermycourse.service.ts
+++ src/app/learner/pages/learner-my-course/learnermycourse.service.ts
@@ -12,26 +12,13 @@ userObjId: any;
 userDetail: any;
 
   constructor(private apollo: Apollo, private gs: GlobalServiceService, public route: Router, public alert: AlertServiceService) {
-    if (this.route.url !== '/' && this.route.url !== '/Learner/login' && this.route.url !== '/Learner' &&
-    this.route.url !== '/Admin/login') {
+
 
     this.userDetail = JSON.parse(localStorage.getItem('UserDetails')) || JSON.parse(sessionStorage.getItem('UserDetails')) || null;
-    // console.log(userDetail);
-    if ((this.userDetail != null && this.userDetail !== undefined)) {
-      return this.userDetail;
-      // } else if ((adminDetails != null || adminDetails !== undefined) && role === 'admin') {
-      //   return adminDetails;
-    } else {
-      this.alert.openAlert('Logged Out!', 'You have been logged out. Please login to continue');
-      this.route.navigate(['/Learner/login']);
-    }
-  }
    }
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
 
 this.userObjId = this.userDetail._id;
-console.log('learnerDetail', learnerDetail);
-console.log('userObjId', this.userObjId);
 return this.getCountForCategories(this.userObjId);
   }
   getCountForCategories(userObjId) {
