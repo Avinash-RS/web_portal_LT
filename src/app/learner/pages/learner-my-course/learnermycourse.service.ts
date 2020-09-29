b380125f39bf680e1484e568dc40b922188cd149
manikanda-p <68428925+manikanda-p@users.noreply.github.com>
2020-09-29 15:53:03 +0530

    Fix : login issue fixed for route method

--- src/app/learner/pages/learner-my-course/learnermycourse.service.ts
+++ src/app/learner/pages/learner-my-course/learnermycourse.service.ts
@@ -1,19 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { getCountForCategories } from '../../services/operations/learner_query';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
 import { GlobalServiceService } from '@core/services/handlers/global-service.service';
+import { AlertServiceService } from '@core/services/handlers/alert-service.service';
 @Injectable({
   providedIn: 'root'
 })
 export class LearnermycourseService implements Resolve<any> {
 userObjId: any;
-  constructor(private apollo: Apollo, private gs: GlobalServiceService) {
+userDetail: any;
 
+  constructor(private apollo: Apollo, private gs: GlobalServiceService, public route: Router, public alert: AlertServiceService) {
+    if (this.route.url !== '/' && this.route.url !== '/Learner/login' && this.route.url !== '/Learner' &&
+    this.route.url !== '/Admin/login') {
+
+    this.userDetail = JSON.parse(localStorage.getItem('UserDetails')) || JSON.parse(sessionStorage.getItem('UserDetails')) || null;
+    // console.log(userDetail);
+    if ((this.userDetail != null && this.userDetail !== undefined)) {
+      return this.userDetail;
+      // } else if ((adminDetails != null || adminDetails !== undefined) && role === 'admin') {
+      //   return adminDetails;
+    } else {
+      this.alert.openAlert('Logged Out!', 'You have been logged out. Please login to continue');
+      this.route.navigate(['/Learner/login']);
+    }
+  }
    }
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-const learnerDetail = JSON.parse(localStorage.getItem('UserDetails'));
-this.userObjId = learnerDetail._id;
+
+this.userObjId = this.userDetail._id;
 console.log('learnerDetail', learnerDetail);
 console.log('userObjId', this.userObjId);
 return this.getCountForCategories(this.userObjId);
