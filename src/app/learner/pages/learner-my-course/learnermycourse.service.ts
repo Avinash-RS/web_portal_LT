1436360995fac6cde927fe8a6b2b9e3e3c8a1d3d
manikanda-p <68428925+manikanda-p@users.noreply.github.com>
2020-09-29 14:49:38 +0530

    Fix : blocker issue

--- src/app/learner/pages/learner-my-course/learnermycourse.service.ts
+++ src/app/learner/pages/learner-my-course/learnermycourse.service.ts
@@ -2,15 +2,20 @@ import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { getCountForCategories } from '../../services/operations/learner_query';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { GlobalServiceService } from '@core/services/handlers/global-service.service';
 @Injectable({
   providedIn: 'root'
 })
 export class LearnermycourseService implements Resolve<any> {
 userObjId: any;
-  constructor(private apollo: Apollo) { }
+  constructor(private apollo: Apollo, private gs: GlobalServiceService) {
+
+   }
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
 const learnerDetail = JSON.parse(localStorage.getItem('UserDetails'));
 this.userObjId = learnerDetail._id;
+console.log('learnerDetail', learnerDetail);
+console.log('userObjId', this.userObjId);
 return this.getCountForCategories(this.userObjId);
   }
   getCountForCategories(userObjId) {
