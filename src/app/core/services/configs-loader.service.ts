e219a65ab658c6304b58a39a3688430ba256d5b6
Mythreyi <mythreyi@gmail.com>
2020-10-22 11:59:27 +0530

    loaders and disbale send button done in df

--- src/app/core/services/configs-loader.service.ts
+++ src/app/core/services/configs-loader.service.ts
@@ -5,68 +5,68 @@ import { HttpClient, HttpBackend } from '@angular/common/http';
   providedIn: 'root'
 })
 /**
- * Retreive the values from the configs.json file under the assets 
- * directory. At release time, azure pipelines replaces the values 
- * in the file based on the variables configured in azure devops. 
- * 
- * Throughout the app, other services can reference the configs via this 
+ * Retreive the values from the configs.json file under the assets
+ * directory. At release time, azure pipelines replaces the values
+ * in the file based on the variables configured in azure devops.
+ *
+ * Throughout the app, other services can reference the configs via this
  * service, instead of referecing the environment.ts file so that we can deploy
- * the same artifact to multiple environments without having to build multiple 
- * times. 
+ * the same artifact to multiple environments without having to build multiple
+ * times.
  */
 export class ConfigsLoaderService {
 
-   // If you app has an interceptor and you don't want it to intercept the 
-   // loading of the file, you can use a seprate HttpClient instance. 
-   // For instance, in my real application,  I have an interceptor that 
-   // intercepts and inserts access token to the header of an http request. 
-   // I don't want the interceptor to intercept the loading file request 
-   // because it would require an access token with is not available at 
-   // initialization time.  
-   private httpClient: HttpClient;
-   private configs: Configs;
+  // If you app has an interceptor and you don't want it to intercept the
+  // loading of the file, you can use a seprate HttpClient instance.
+  // For instance, in my real application,  I have an interceptor that
+  // intercepts and inserts access token to the header of an http request.
+  // I don't want the interceptor to intercept the loading file request
+  // because it would require an access token with is not available at
+  // initialization time.
+  private httpClient: HttpClient;
+  private configs: Configs;
 
-   constructor(handler: HttpBackend) {
-    this.httpClient = new HttpClient(handler); 
+  constructor(handler: HttpBackend) {
+    this.httpClient = new HttpClient(handler);
   }
 
   get ApiUrl() {
-    return this.configs.apiUrl; 
+    return this.configs.apiUrl;
   }
   get ScormUrl() {
-    return this.configs.scormUrl; 
+    return this.configs.scormUrl;
   }
   get ApiUrlImg() {
-    return this.configs.apiUrlImg; 
+    return this.configs.apiUrlImg;
   }
   get CreateCourseApi() {
-    return this.configs.createCourseApi; 
+    return this.configs.createCourseApi;
   }
   get Wcaapiurl() {
-    return this.configs.wcaapiurl; 
+    return this.configs.wcaapiurl;
   }
   get Domain() {
-    return this.configs.domain; 
+    return this.configs.domain;
   }
 
 
-  public async loadConfigs() : Promise<any> {
+  public async loadConfigs(): Promise<any> {
     return this.httpClient.get('assets/configs.json').pipe(settings => settings)
       .toPromise()
       .then(settings => {
-        this.configs = settings as Configs; 
+        this.configs = settings as Configs;
       });
   }
 
 }
 
 export interface Configs {
-//   ApiUrl: string, 
-//   ConfigsLoadedFrom: string,
-  apiUrl: string,
-  scormUrl: string, 
-  apiUrlImg: string, 
-  createCourseApi: string,
-  wcaapiurl: string, 
-  domain : string, 
+  //   ApiUrl: string,
+  //   ConfigsLoadedFrom: string,
+  apiUrl: string;
+  scormUrl: string;
+  apiUrlImg: string;
+  createCourseApi: string;
+  wcaapiurl: string;
+  domain: string;
 }
