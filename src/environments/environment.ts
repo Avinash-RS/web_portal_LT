9a519cd5c4e62421917bbdfa0d4e4c26bc36eeff
Rajesh <rajesh.kumar@cintanatech.com>
2020-10-31 18:32:53 +0530

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,45 +1,32 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+// This file can be replaced during build by using the fileReplacements array.
+// ng build --prod replaces environment.ts with environment.prod.ts.
+// The list of file replacements can be found in angular.json.
 
+export const environment = {
+  production: false,
+  apiUrl: 'http://facade.southindia.cloudapp.azure.com:3000/',
+  scormUrl: 'http://scorm.southindia.cloudapp.azure.com:8089/',
+  apiUrlImg: 'http://facade.southindia.cloudapp.azure.com:3000/',
+  createCourseApi: 'http://course.southindia.cloudapp.azure.com:3002/',
+  wcaapiurl: 'http://wca.southindia.cloudapp.azure.com:9001/',
+  domain : 'LXP',
+  analytics: 'UA-171656647-1',
+  systemIp : 'http://api.ipify.org/?format=json',
+  socketio : 'http://20.40.0.83:3009'
+};
 // export const environment = {
 //   production: false,
-//   apiUrl: 'http://facade.southindia.cloudapp.azure.com:3000/',
-//   scormUrl: 'http://scorm.southindia.cloudapp.azure.com:8089/',
-//   apiUrlImg: 'http://facade.southindia.cloudapp.azure.com:3000/',
-//   createCourseApi: 'http://course.southindia.cloudapp.azure.com:3002/',
-//   wcaapiurl: 'http://wca.southindia.cloudapp.azure.com:9001/',
+//   apiUrl: 'http://20.44.35.112:3000/',
+//   scormUrl:'http://52.140.12.78:8089/',
+//   apiUrlImg:'http://20.44.35.112:3000/',
+//   createCourseApi:'http://52.140.12.145:3002/',
+//   wcaapiurl:'http://20.44.38.161:9001/',
 //   domain : 'LXP',
-//   analytics: 'UA-171656647-1',
 //   systemIp : 'http://api.ipify.org/?format=json',
-//   socketio : 'http://20.40.0.83:3009'
 // };
-export const environment = {
-  // apiUrl: 'https://facade.lntiggnite.com/',
-  // scormUrl: 'https://scorm.lntiggnite.com/',
-  // apiUrlImg: 'https://facade.lntiggnite.com/',
-  // createCourseApi: 'https://course.lntiggnite.com/',
-  // wcaapiurl: 'https://wca.lntiggnite.com/',
-  // apiUrl: 'http://20.44.42.101:3000/',
-  // scormUrl:'http://52.140.8.0:8089/',
-  // apiUrlImg:'http://20.44.42.101:3000/',
-  // createCourseApi:'http://20.44.44.159:3002/',
-  // wcaapiurl:'http://20.44.34.192:9001/',
-  // apiUrl: 'https://facade.lntiggnite.com/',
-  // scormUrl: 'https://scorm.lntiggnite.com/',
-  // apiUrlImg: 'https://facade.lntiggnite.com/',
-  // createCourseApi: 'https://course.lntiggnite.com/',
-  // wcaapiurl: 'https://wca.lntiggnite.com/',
-  apiUrl: 'http://20.44.41.200:3000/',
-  scormUrl:'http://20.44.36.116:8089/',
-  apiUrlImg:'http://20.44.41.200:3000/',
-  createCourseApi:'http://20.44.41.55:3002/',
-  wcaapiurl:'http://20.44.34.169:9001/',
-socketio : 'http://20.40.0.83:3009'
-};
 /*
  * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ * to ignore zone related error stack frames such as zone.run, zoneDelegate.invokeTask.
  *
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
