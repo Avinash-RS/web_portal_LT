0bfc1a6c0a6aab2f9ca10ca6625bdbcf0c859e9c
Rajesh Kumar Ranjan <RAJESH-KUMAR-RANJAN@lntecc.com>
2020-11-06 12:32:19 +0000

    Updated environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,21 @@
 
 export const environment = {
   production: false,
-  /*apiUrl: 'http://20.44.35.112:3000/',
+  apiUrl: 'http://facade.southindia.cloudapp.azure.com:3000/',
+  scormUrl: 'http://scorm.southindia.cloudapp.azure.com:8089/',
+  apiUrlImg: 'http://facade.southindia.cloudapp.azure.com:3000/',
+  createCourseApi: 'http://course.southindia.cloudapp.azure.com:3002/',
+  wcaapiurl: 'http://wca.southindia.cloudapp.azure.com:9001/',
+  domain : 'LXP',
+  analytics: 'UA-171656647-1',
+  systemIp : 'http://api.ipify.org/?format=json',
+  socketio : 'http://20.40.0.83:3009'
+  /*production: false,
+  apiUrl: 'http://20.44.35.112:3000/',
   scormUrl:'http://52.140.12.78:8089/',
   apiUrlImg:'http://20.44.35.112:3000/',
   createCourseApi:'http://52.140.12.145:3002/',
-  wcaapiurl:'http://20.44.38.161:9001/',*/
+  wcaapiurl:'http://20.44.38.161:9001/',
   apiUrl: 'http://facadeqa.southindia.cloudapp.azure.com:3000/',
   scormUrl:'http://scormqa.southindia.cloudapp.azure.com:8089/',
   apiUrlImg:'http://facadeqa.southindia.cloudapp.azure.com:3000/',
@@ -17,7 +27,7 @@ export const environment = {
   analytics: 'UA-171656647-1',
   domain : 'LXP',
   systemIp : 'http://api.ipify.org/?format=json',
-  socketio : 'http://20.40.0.83:3009'
+  socketio : 'http://20.40.0.83:3009'*/
 };
 
 /*
