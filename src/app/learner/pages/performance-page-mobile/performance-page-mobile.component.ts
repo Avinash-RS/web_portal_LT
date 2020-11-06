926d127a6156d06ea42530d9bf5db100e1bfe734
guru <guru.venkadesh@cintanatech.com>
2020-11-06 20:46:26 +0530

    changes done

--- src/app/learner/pages/performance-page-mobile/performance-page-mobile.component.ts
+++ src/app/learner/pages/performance-page-mobile/performance-page-mobile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, Input, Output, EventEmitter } from "@angular/core";
+import { Component, OnInit, ViewChild, TemplateRef, Input, Output, EventEmitter } from "@angular/core";
 import { GlobalServiceService } from "@core/services/handlers/global-service.service";
 import { ActivatedRoute, Router } from "@angular/router";
 import { CommonServicesService } from "@core/services/common-services.service";
@@ -46,6 +46,7 @@ export class PerformancePageMobileComponent implements OnInit {
   submitType: string;
   videoRecord = false;
   itrationDataSend: any;
+  docpath: any = null;
 
   constructor(
     private commonServices: CommonServicesService,
@@ -221,4 +222,30 @@ export class PerformancePageMobileComponent implements OnInit {
     this.itrationData = itration;
     this.videoInput.nativeElement.click();
   }
+
+  closedialogbox() {
+    this.dialog.closeAll();
+    // this.addThreadForm?.reset();
+  }
+
+  playVideo(templateRef: TemplateRef<any>,  path) {
+  this.dialog.open(templateRef, {
+    width: '100%',
+    height: '100%',
+    closeOnNavigation: true,
+    disableClose: true,
+  });
+  this.previewDoc = path;
+}
+
+previewDoc(templateRef: TemplateRef<any>, path) {
+  console.log('path', path);
+  this.dialog.open(templateRef, {
+    width: '100%',
+    height: '100%',
+    closeOnNavigation: true,
+    disableClose: true,
+  });
+  this.docpath = path;
+}
 }
