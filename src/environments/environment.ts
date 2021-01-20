dbc6a955472d31399965adeffa5675de43a796a3
Rajesh Kumar Ranjan <RAJESH-KUMAR-RANJAN@lntecc.com>
2021-01-20 07:17:15 +0000

    Updated environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://facade.southindia.cloudapp.azure.com:3000/',
+  apiUrl: 'https://devfacade.lntiggnite.com/',
   scormUrl: 'http://scorm.southindia.cloudapp.azure.com:8089/',
-  apiUrlImg: 'http://facade.southindia.cloudapp.azure.com:3000/',
-  createCourseApi: 'http://course.southindia.cloudapp.azure.com:3002/',
-  wcaapiurl: 'http://wca.southindia.cloudapp.azure.com:9001/',
+  apiUrlImg: 'https://devfacade.lntiggnite.com/',
+  createCourseApi: 'https://devcourses.lntiggnite.com/',
+  wcaapiurl: 'https://devwca.lntiggnite.com/',
   domain : 'LXP',
   analytics: 'UA-171656647-1',
   systemIp : 'http://api.ipify.org/?format=json',
-  socketio : 'http://20.40.0.83:3009',
+  socketio : 'https://devsocket.lntiggnite.com/',
   captachaSiteKey: '6Ldu8h8aAAAAAEV33ZBs-xCR4hHIzPuvfLjkG-oO'
 };
 
