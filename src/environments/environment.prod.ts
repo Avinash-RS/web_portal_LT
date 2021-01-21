7414669db53f0840c934b10be35a6cec5152cf50
Pradeep Kumar J <PRADEEP-J@lntecc.com>
2021-01-21 05:39:29 +0000

    Updated environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-export const environment = {
+/*export const environment = {
   production: false,
   apiUrl: 'http://facade.southindia.cloudapp.azure.com:3000/',
   scormUrl: 'http://scorm.southindia.cloudapp.azure.com:8089/',
@@ -9,5 +9,23 @@ export const environment = {
   analytics: 'UA-171656647-1',
   systemIp : 'http://api.ipify.org/?format=json',
   socketio : 'http://20.40.0.83:3009',
+  captachaSiteKey: '6Ldu8h8aAAAAAEV33ZBs-xCR4hHIzPuvfLjkG-oO'
+};
+*/
+// This file can be replaced during build by using the fileReplacements array.
+// ng build --prod replaces environment.ts with environment.prod.ts.
+// The list of file replacements can be found in angular.json.
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://facade.lntiggnite.com/',
+  scormUrl:'https://scorm.lntiggnite.com/',
+  apiUrlImg:'https://facade.lntiggnite.com/',
+  createCourseApi:'https://course.lntiggnite.com/',
+  wcaapiurl:'https://wca.lntiggnite.com/',
+  analytics: 'UA-171656647-1',
+  domain : 'LXP',
+  systemIp : 'https://api.ipify.org/?format=json',
+  socketio : 'https://socketio.lntiggnite.com',
   captachaSiteKey: '6Ldu8h8aAAAAAEV33ZBs-xCR4hHIzPuvfLjkG-oO'
 };
