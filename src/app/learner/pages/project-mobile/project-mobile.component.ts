4a15a3cd827b6f466b1e27b2c5938ebfc28a1988
Karthikeyan <karthikeyanb@lntecc.com>
2021-01-28 18:02:54 +0530

    performance fix

--- src/app/learner/pages/project-mobile/project-mobile.component.ts
+++ src/app/learner/pages/project-mobile/project-mobile.component.ts
@@ -322,6 +322,7 @@ export class ProjectMobileComponent implements OnInit {
             height: '100%',
             closeOnNavigation: true,
             disableClose: true,
+            panelClass: 'popupModalContainer'
           });
           this.previewDoc = path;
       } else {
