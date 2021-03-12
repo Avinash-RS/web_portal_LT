c082f0087661a049d3c219d83b83dc5ad75c862d
Gokul <gokulappaswamy@lntecc.com>
2021-03-12 20:03:48 +0530

    api integration started

--- src/app/learner/pages/ask-questions/ask-questions.component.ts
+++ src/app/learner/pages/ask-questions/ask-questions.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, TemplateRef } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
+import { Router } from "@angular/router";
+import { GlobalServiceService } from "@core/services/handlers/global-service.service";
+import { LearnerServicesService } from "@learner/services/learner-services.service";
 
 @Component({
   selector: "app-ask-questions",
@@ -10,8 +13,26 @@ import { MatDialog } from "@angular/material/dialog";
 export class AskQuestionsComponent implements OnInit {
   allQuestionList = [{"filteredValue":{"question":{"que_id":6,"que":"sdfasdfasdafsadf","ans":null,"askDate":"6th Mar, 2021","ansDate":null}}},{"filteredValue":{"question":{"que_id":5,"que":"gokul test for 123 lakdjlf","ans":null,"askDate":"6th Mar, 2021","ansDate":null}}},{"filteredValue":{"question":{"que_id":4,"que":"rekost alkul  lakjldf  laidlflakl aldifan","ans":null,"askDate":"6th Mar, 2021","ansDate":null}}},{"filteredValue":{"question":{"que_id":3,"que":"aguliar","ans":null,"askDate":"6th Mar, 2021","ansDate":null}}},{"filteredValue":{"question":{"que_id":2,"que":"hello boy","ans":null,"askDate":"6th Mar, 2021","ansDate":null}}},{"filteredValue":{"question":{"que_id":1,"que":"hello boy","ans":null,"askDate":"6th Mar, 2021","ansDate":null}}}]
   questionText: string;
-  constructor(private dialog: MatDialog,) { 
-    
+  batchId: string;
+  localStoCourseid: string;
+  courseid: any;
+  userDetail: any;
+  constructor(private dialog: MatDialog,
+    public Lservice: LearnerServicesService,
+    public route: Router,
+    private gs: GlobalServiceService,
+    ) { 
+
+      const detail = (this.route.getCurrentNavigation() && this.route.getCurrentNavigation().extras &&
+      this.route.getCurrentNavigation().extras.state && this.route.getCurrentNavigation().extras.state.detail);
+    if(detail===undefined){
+        this.batchId = localStorage.getItem('currentBatchId')
+      }else{
+        this.batchId = detail.batch_id
+      }
+      this.userDetail = this.gs.checkLogout();
+      this.localStoCourseid = localStorage.getItem('Courseid');
+      this.courseid = detail && detail.id || this.localStoCourseid;
   }
 
   ngOnInit() {
@@ -26,6 +47,12 @@ export class AskQuestionsComponent implements OnInit {
   closeOnNavigation: true,
   //disableClose: true,
 });
+}
+
+getPlayerModuleTopic(){
+  this.Lservice.playerModuleAndTopic(this.courseid, this.userDetail.user_id).subscribe((data: any) => {
+    console.log(data)
+  })
 }
 
   goBack(){
