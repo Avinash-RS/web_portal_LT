0d169fb18fa3381e46c29b03a3c7b2b3636562f8
mmanju83 <mmanju83@gmail.com>
2021-07-06 17:27:51 +0530

    Calendar activity calendar UI design
    Calendar activity calendar UI design Created

--- src/app/learner/pages/calendar-activity/calendar-activity.component.ts
+++ src/app/learner/pages/calendar-activity/calendar-activity.component.ts
@@ -1,10 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { CalendarEvent, CalendarView,CalendarMonthViewDay } from 'angular-calendar'; 
+import { Component, OnInit, Injectable } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { CalendarEvent, CalendarView,CalendarMonthViewDay, DateFormatterParams, CalendarDateFormatter } from 'angular-calendar'; 
+
+@Injectable()
+export class CustomDateFormatter extends CalendarDateFormatter {
+
+  public monthViewColumnHeader({ date, locale }: DateFormatterParams): string {
+    return formatDate(date, 'EEE', locale);
+  }
+ 
+  public weekViewColumnHeader({ date, locale }: DateFormatterParams): string {
+    return formatDate(date, 'EEE', locale);
+  } 
+}
 
 @Component({
   selector: 'app-calendar-activity',
   templateUrl: './calendar-activity.component.html',
-  styleUrls: ['./calendar-activity.component.scss']
+  styleUrls: ['./calendar-activity.component.scss'],
+  providers: [
+    {
+      provide: CalendarDateFormatter,
+      useClass: CustomDateFormatter,
+    },
+  ],
 })
 export class CalendarActivityComponent implements OnInit {
   events: CalendarEvent[];
