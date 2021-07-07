8d67ab48b17b8df5831510671b37baea8b08f0b9
Karthikeyan <karthikeyanb@lntecc.com>
2021-07-07 16:03:09 +0530

    alignment fixes

--- src/app/learner/pages/learner-new-my-course/learner-new-my-course.component.ts
+++ src/app/learner/pages/learner-new-my-course/learner-new-my-course.component.ts
@@ -21,6 +21,8 @@ export class LearnerNewMyCourseComponent implements OnInit {
   isReadMore = true;
   show = true;
   innerWidth: number;
+  expandcollapse = true;
+
   //Carousel
   missedTopicsKnowledgeCheck: OwlOptions = {
     loop: true,
@@ -47,10 +49,10 @@ export class LearnerNewMyCourseComponent implements OnInit {
     },
     nav: false
   }
-  expandcollapse = true;
 
   constructor() { 
   }
+  
   @HostListener('window:resize', ['$event'])
 
   ngOnInit() {
