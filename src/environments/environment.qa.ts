edacca35f0adb386aa4783c8ba46794fbb00682a
Naren <naren-s@lntecc.com>
2021-07-17 11:37:42 +0530

    10673,10675 - Chart API integration & Teacher's community redirection

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -10,6 +10,6 @@ export const environment = {
   socketio : '20.40.2.200:3009',
   captachaSiteKey: '6Ldu8h8aAAAAAEV33ZBs-xCR4hHIzPuvfLjkG-oO',
   galleryURL : 'https://lmsproductionpremium.z23.web.core.windows.net',
-  blobKey: '?sv=2018-03-28&ss=b&srt=sco&sp=racwdl&st=2021-04-16T11%3A32%3A54Z&se=2171-04-16T11%3A37%3A54Z&spr=https%2Chttp&sig=9m73poSUv3%2FE%2BsrRJEOjA8dmL50w4073u%2B3VgsN%2FaDI%3D'
-
+  blobKey: '?sv=2018-03-28&ss=b&srt=sco&sp=racwdl&st=2021-04-16T11%3A32%3A54Z&se=2171-04-16T11%3A37%3A54Z&spr=https%2Chttp&sig=9m73poSUv3%2FE%2BsrRJEOjA8dmL50w4073u%2B3VgsN%2FaDI%3D',
+  teachercommunity: 'https://engineers.lntiggnite.com/teachercommunity/'
 };
