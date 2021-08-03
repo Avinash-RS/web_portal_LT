ecb40a50c9c26fa900083a710f20cf950cb3a3c2
prs <contactprasanth.r@gmail.com>
2021-08-03 18:11:51 +0530

    Bug 11043: Learner - Self learning progression report : Provide % on self learning pie chart. Align "Question", "Correct" and "Incorrect" in parallel to above line items in progression card.

--- src/app/learner/pages/progression-report/progression-report.component.ts
+++ src/app/learner/pages/progression-report/progression-report.component.ts
@@ -322,12 +322,13 @@ export class ProgressionReportComponent implements OnInit {
           },
           datalabels: {
             formatter: (value, ctx) => {
-              let sum = 0;
-              let dataArr = ctx.chart.data.datasets[0].data;
-              dataArr.map(data => {
-                  sum += data;
-              });
-              let percentage = (value*100 / sum)+"%";
+              // let sum = 0;
+              // let dataArr = ctx.chart.data.datasets[0].data;
+              // dataArr.map(data => {
+              //     sum += data;
+              // });
+              // let percentage = (value*100 / sum)+"%";
+              let percentage = value + "%";
               return percentage;
           },
 
