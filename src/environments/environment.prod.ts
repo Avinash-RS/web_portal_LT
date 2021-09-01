ad42a93d45446224a75db0e0b9f7367058587052
Alagappan <alagappan.palaniyappan@lntecc.com>
2021-09-01 18:08:48 +0530

    made changes in env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -19,9 +19,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://devfacade.lntiggnite.com/',
+  apiUrl: '/gateway',
   scormUrl: 'https://devscorm.lntiggnite.com/',
-  apiUrlImg: 'https://devfacade.lntiggnite.com/',
+  apiUrlImg: '/gateway',
   createCourseApi: 'https://devcourses.lntiggnite.com/',
   wcaapiurl: 'https://devwca.lntiggnite.com/',
   domain : 'LXP',
