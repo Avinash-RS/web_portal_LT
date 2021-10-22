843aa7c07af55fd9ccef220c71c6506658878f64
naren-s <naren-s@lntecc.com>
2021-10-22 15:42:43 +0530

    Invisible captcha implementation

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,19 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://devfacade.lntiggnite.com/',
+  apiUrl: 'http://192.168.87.187:3000/',
   scormUrl: 'https://devscorm.lntiggnite.com/',
-  apiUrlImg: 'https://devfacade.lntiggnite.com/',
+  apiUrlImg: 'http://192.168.87.187:3000/',
   createCourseApi: 'https://devcourses.lntiggnite.com/',
   wcaapiurl: 'https://devwca.lntiggnite.com/',
   domain : 'LXP',
   analytics: 'UA-171656647-1',
   systemIp : 'http://api.ipify.org/?format=json',
   socketio : 'https://devsocket.lntiggnite.com/',
-  captachaSiteKey: '6Leia8YbAAAAAB2jaH0YtNRQjQlr8OATGngAIBkk',
+  // Visible captcha key
+  // captachaSiteKey: '6Leia8YbAAAAAB2jaH0YtNRQjQlr8OATGngAIBkk',
+  // invisble captcha key
+  captachaSiteKey: '6LfFoOccAAAAALSltFKyGqS9KwB3p9bJIOGaZ2QS',
   galleryURL : 'https://lxpdevstorage.z29.web.core.windows.net',
   blobKey: '?sv=2018-03-28&ss=b&srt=sco&sp=racwdl&st=2021-04-12T03%3A36%3A39Z&se=2171-04-12T03%3A41%3A39Z&spr=https%2Chttp&sig=jl7SfT41958EUB7YDb48xwU65cZcsnTN8vdttEhBad8%3D',
   teachercommunity: 'https://engineers.lntiggnite.com/teachercommunity/',
