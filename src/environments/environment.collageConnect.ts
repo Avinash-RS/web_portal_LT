e8cf791de25059544819125c58ebcc7cab6c30b1
Narendran <NAREN-S@lntecc.com>
2021-10-25 11:29:40 +0000

    Updated environment.collageConnect.ts

--- src/environments/environment.collageConnect.ts
+++ src/environments/environment.collageConnect.ts
@@ -10,7 +10,10 @@ export const environment = {
     systemIp : 'http://api.ipify.org/?format=json',
     orgId: '' , // saveetha,
     socketio : '20.44.32.238:3009',
-    captachaSiteKey: '6Ldu8h8aAAAAAEV33ZBs-xCR4hHIzPuvfLjkG-oO',
+      // Visible captcha key
+  // captachaSiteKey: '6Leia8YbAAAAAB2jaH0YtNRQjQlr8OATGngAIBkk',
+  // invisble captcha key
+  captachaSiteKey: '6Lf-qfEcAAAAAH2zsrdDz1K6DmUOHjgHzGmH3PN7',
     galleryURL : 'https://lxpdevstorage.z29.web.core.windows.net',
     blobKey: '?sv=2018-03-28&ss=b&srt=sco&sp=racwdl&st=2021-04-12T03%3A36%3A39Z&se=2171-04-12T03%3A41%3A39Z&spr=https%2Chttp&sig=jl7SfT41958EUB7YDb48xwU65cZcsnTN8vdttEhBad8%3D',
     teachercommunity: 'https://engineers.lntiggnite.com/teachercommunity/',
