cce18c008aa371d892097f51a16af606fa03d508
Aishwarya <aishwarya.r@lntecc.com>
2022-03-03 18:51:37 +0530

    Upskill calendar UI

--- src/app/learner/pages/upskill-calendar/upskill-calendar.component.ts
+++ src/app/learner/pages/upskill-calendar/upskill-calendar.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CalendarEvent, CalendarView,CalendarMonthViewDay } from 'angular-calendar';
 @Component({
   selector: 'app-upskill-calendar',
   templateUrl: './upskill-calendar.component.html',
   styleUrls: ['./upskill-calendar.component.scss']
-})
-export class UpskillCalendarComponent implements OnInit {
+  })
 
+export class UpskillCalendarComponent implements OnInit {
+  view: CalendarView = CalendarView.Month;
+  selectedMonthViewDay: CalendarMonthViewDay;
+  selectedDays: any = [];
+  CalendarView = CalendarView;
+  viewDate: Date = new Date();
+
+  setView(view: CalendarView) {
+    this.view = view;
+  }
   constructor() { }
 
   ngOnInit() {
