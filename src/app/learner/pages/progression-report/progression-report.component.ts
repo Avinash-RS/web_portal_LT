de5fec64bca983528bed697577eea52f8f839364
thrivikraman.r <thrivikraman.r@lntecc.com>
2022-03-10 17:05:58 +0530

    Task 19333: Upskill calendar Activity Filter Clear All issue. Bug 19321: Self Learning Report - Loader when the learner has not started the course.

--- src/app/learner/pages/progression-report/progression-report.component.ts
+++ src/app/learner/pages/progression-report/progression-report.component.ts
@@ -344,7 +344,7 @@ export class ProgressionReportComponent implements OnInit {
     this.showProgReport = false
     this.learnerService.getProgressionData(this.userId, this.courseId).subscribe((data: any) => {
       this.apidata = data.data.getCourseReportByUserid.data.module;
-      if(this.apidata[0]?.moduleName){
+      if(this.apidata && this.apidata[0]?.moduleName){
         this.showWeek = false;
       } else {
         this.showWeek = true;
