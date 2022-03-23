3279efe08e5afc1115f0b7d257333487fa88aa7e
Gokul Appaswamy <77961879+GokulALT@users.noreply.github.com>
2022-03-23 10:31:27 +0530

    Upskill ToC hiding fixes

--- src/app/learner/pages/learner-new-my-course/learner-new-my-course.component.ts
+++ src/app/learner/pages/learner-new-my-course/learner-new-my-course.component.ts
@@ -957,7 +957,7 @@ goToCourse(c){
   localStorage.setItem('persentage', c && c.coursePlayerStatus && c.coursePlayerStatus.course_percentage ? c.coursePlayerStatus.course_percentage : '');
   localStorage.setItem('currentBatchId', c.batchid);
 
-  localStorage.setItem('resumeData', JSON.stringify({'link':c.link,'lastModule':c.lastModule,'lastTopic':c.lastTopic,'module_id':c.module_id,'topic_id':c.topic_id,'checklevel':c.checklevel,'course_status':c.course_status}));
+  localStorage.setItem('resumeData', JSON.stringify({'link':c.link,'lastModule':c.lastModule,'lastTopic':c.lastTopic,'module_id':c.module_id,'topic_id':c.topic_id,'checklevel':c.checklevel,'course_status':c.course_status,'toc':c.toc}));
 
   this.router.navigateByUrl('/Learner/courseDetail', { state: { detail } });
 }
