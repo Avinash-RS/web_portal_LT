16ee81e976027195972b07a9aa6292ce765a6a74
Rajesh Kumar Ranjan <rajesh.ranjan@lntedutech.com>
2022-04-04 09:47:43 +0000

    Updated environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -20,15 +20,15 @@
 export const environment = {
   production: false,
   appVersion: require('../../package.json').version + '-prod',
-  apiUrl: 'https://devfacade.lntiggnite.com/',
-  scormUrl: 'https://devscorm.lntiggnite.com/',
-  apiUrlImg: 'https://devfacade.lntiggnite.com/',
-  createCourseApi: 'https://devcourses.lntiggnite.com/',
-  wcaapiurl: 'https://devwca.lntiggnite.com/',
+  apiUrl: 'https://devfacadept.lntiggnite.com/',
+  scormUrl: 'https://devscormpt.lntiggnite.com/',
+  apiUrlImg: 'https://devfacadept.lntiggnite.com/',
+  createCourseApi: 'https://devcoursespt.lntiggnite.com/',
+  wcaapiurl: 'https://devwcapt.lntiggnite.com/',
   domain : 'LXP',
   analytics: 'UA-171656647-1',
   systemIp : 'https://api.ipify.org/?format=json',
-  socketio : 'https://devsocket.lntiggnite.com/',
+  socketio : 'https://devsocketpt.lntiggnite.com/',
     // Visible captcha key
   // captachaSiteKey: '6Leia8YbAAAAAB2jaH0YtNRQjQlr8OATGngAIBkk',
   // invisble captcha key
@@ -37,6 +37,6 @@ export const environment = {
   blobKey: '?sv=2018-03-28&ss=b&srt=sco&sp=racwdl&st=2021-04-12T03%3A36%3A39Z&se=2171-04-12T03%3A41%3A39Z&spr=https%2Chttp&sig=jl7SfT41958EUB7YDb48xwU65cZcsnTN8vdttEhBad8%3D',
   teachercommunity: 'https://engineers.lntiggnite.com/teachercommunity/',
   gaTrackingId: 'G-6N6X7SQTXH',
-  botUrl :'https://devfaqbot.lntiggnite.com/',
+  botUrl :'https://devfaqbotpt.lntiggnite.com/',
   resourcelinkurl : 'https://upskillresourcefiles.lntedutech.com/'
 };
