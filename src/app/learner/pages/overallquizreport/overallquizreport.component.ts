45575c9afd83e97dd52f012d8f8f169c577680f0
thrivikraman.r <thrivikraman.r@lntecc.com>
2022-05-17 18:04:05 +0530

    Task 21473: Learner Mile stone Course Quiz report UI implementation

--- src/app/learner/pages/overallquizreport/overallquizreport.component.ts
+++ src/app/learner/pages/overallquizreport/overallquizreport.component.ts
@@ -100,9 +100,71 @@ export class OverallquizreportComponent implements OnInit {
     {color: 'avg', label: '31-70% Average'},
     {color:' poor', label: '0-30% poor'}
   ];
+  defaultColDef = {
+    resizable: false,
+    floatingFilter: false,
+    enableColResize: false,
+    sortable: false,
+    lockPosition: true,
+    suppressMenu: true,
+    unSortIcon: true,
+  };
+  rows = [
+    {quiz_name:'asasa',start_date:'02-02-2022',no_of_question:'10',correct_answer:'9',score:'90',status:'Good'},
+    {quiz_name:'sdsds',start_date:'02-02-2022',no_of_question:'50',correct_answer:'5',score:'50',status:'Average'},
+    {quiz_name:'sdsds',start_date:'02-02-2022',no_of_question:'20',correct_answer:'2',score:'20',status:'Poor'},
+  ]
+  cols = [
+    { headerName: 'Quiz Topics', field: 'quiz_name', width: 230, tooltipField: 'quiz_name',
+    cellRenderer: (params) => {
+      if (params?.data) {
+        return `<div class="countWrapper d-flex align-items-center">
+        <div class="icon"><i class="lxp-Quiz_Report"></i></div>
+        <span class="count">${params?.data?.quiz_name}</span>
+        </div>`;
+      } else {
+        return '';
+      }
+    }},
+    { headerName: 'Date', field: 'start_date', minWidth: 90, width: 90,
+    cellRenderer: (params) => {
+      if (params?.data?.start_date) {
+        // let result = params?.data?.start_date.substring(0, 10);
+        // return moment(result).local().format('DD/MM/YY');
+        return params?.data?.start_date;
+      } else {
+        return '';
+      }
+    }
+  },
+    { headerName: 'Total Questions', field: 'no_of_question', minWidth: 85, width: 85},
+    { headerName: 'No. of Correct Answers', field: 'correct_answer', minWidth: 110, width: 110},
+    { headerName: 'Score', field: 'score',width: 65, minWidth: 65,
+    cellRenderer: (params) => {
+      if (params?.data?.score) {
+        return params?.data?.score + '%';
+      }
+    }},
+    { headerName: 'Status', field: 'status', minWidth: 60, width: 60, cellClass:'statusClass',
+    cellRenderer: (params) => {
+      if (params?.data?.status === 'Good') {
+        return `<div class="d-flex align-items-center justify-content-center statusBtn good">Good</div>`;
+      } else if (params?.data?.status === 'Average') {
+        return `<div class="d-flex align-items-center justify-content-center statusBtn avg">Average</div>`;
+      } else {
+        return `<div class="d-flex align-items-center justify-content-center statusBtn poor">Poor</div>`;
+      }
+
+    } }
+  ];
+  gridApi: any;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onGridReady(params: any) {
+    this.gridApi = params.api;
+    this.gridApi.sizeColumnsToFit();
+    // this.geTQuizData();
+  }
 }
