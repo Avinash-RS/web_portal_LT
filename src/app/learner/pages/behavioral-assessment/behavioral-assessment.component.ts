d00e7adf6f49512d7c9eea16d16e73a031e12774
thrivikraman.r <thrivikraman.r@lntecc.com>
2022-05-25 17:32:34 +0530

    Task 21695: Learner Mile stone Behavioural report UI Implementation Task 21696: Learner Mile stone Images to webp convertion

--- src/app/learner/pages/behavioral-assessment/behavioral-assessment.component.ts
+++ src/app/learner/pages/behavioral-assessment/behavioral-assessment.component.ts
@@ -12,16 +12,38 @@ export class BehavioralAssessmentComponent implements OnInit {
     {score:'3-4-5',color:'green',label:'LESS INCLINED'},
     {score:'6-7-8',color:'yellow',label:'MORE INCLINED'},
     {score:'9-10',color:'orange',label:'STRENGTH'}
-
   ];
+
+  behavioural = [
+    {label:'THOUGHT FACTOR',img:'https://assets.lntedutech.com/application_images/thoughtfactor.webp',description:'Constitutes a group of behavioural competencies that focuses on Cognitive factors like Detail Orientation, Critical thinking and Creative thinking.',stenscore:[
+      {label:"Detail-Oriented",color:'#CBD613'},
+      {label:"Critical Thinking",color:'#95C330'},
+      {label:"Creative Thinking",color:'#687C3C'},
+    ]},
+    {label:'INTERPERSONAL',img:'https://assets.lntedutech.com/application_images/interpersonal.webp',description:'Constitutes a group of behavioural competencies that focuses on Cognitive factors like Detail Orientation, Critical thinking and Creative thinking.',stenscore:[
+      {label:"Communication",color:'#FCC537'},
+      {label:"Positive Attitude",color:'#F59840'},
+      {label:"Teamwork",color:'#E5763E'},
+    ]},
+    {label:'CORE/PERSONAL',img:'https://assets.lntedutech.com/application_images/core.webp',description:'Constitutes a group of behavioural competencies that focuses on Cognitive factors like Detail Orientation, Critical thinking and Creative thinking.',stenscore:[
+      {label:"Self-Reliant",color:'#5DB2DF'},
+      {label:"Growth Mindset",color:'#5783BE'},
+      {label:"Receptiveness",color:'#5461A7'},
+    ]},
+    {label:'EMOTION',img:'https://assets.lntedutech.com/application_images/emotion.webp',description:'Constitutes a group of behavioural competencies that focuses on Cognitive factors like Detail Orientation, Critical thinking and Creative thinking.',stenscore:[
+      {label:"Adaptability",color:'#E5A3F5'},
+      {label:"Anxiety Manage",color:'#CA63E3'},
+      {label:"Self-Awareness",color:'#A63BC0'},
+    ]}
+  ]
   constructor(public dialog:MatDialog) { }
 
   ngOnInit(): void {
   }
   openBenchmarkInfo(template:TemplateRef<any>){
     this.dialog.open(template,{
-      width: "400px",
-      height: "700px",
+      width: "360px",
+      height: "690px",
       position: { right: "0px", bottom: "0px"},
       panelClass: "filter-modal-box",
     })
