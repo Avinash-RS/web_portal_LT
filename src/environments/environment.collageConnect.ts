84d799ad127ba4dca56c7f3e1491ebae770eac82
Narendran Subramani <narendran.subramani@lntedutech.com>
2022-06-08 09:32:13 +0000

    Updated environment.collageConnect.ts

--- src/environments/environment.collageConnect.ts
+++ src/environments/environment.collageConnect.ts
@@ -20,5 +20,5 @@ export const environment = {
     teachercommunity: 'https://engineers.lntiggnite.com/teachercommunity/',
     gaTrackingId: 'G-6N6X7SQTXH',
     resourcelinkurl : 'https://upskillresourcefiles.lntedutech.com/',
-    portalUrl : 'https://portaldev.lntiggnite.com/Home'
+    portalUrl : 'https://microlearndev.lntedutech.com/'
 };
