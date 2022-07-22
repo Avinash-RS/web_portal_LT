bb6276d53adca1b1b629b78b374344e9a8ce9910
Anil M Sirvi <anil.sirvi@lntecc.com>
2022-07-22 18:06:48 +0530

    Task 23635: Self Learning Progression - Weekly Score Table Card UI Implemenattion.

--- src/app/learner/pages/assessment-report-b2c/assessment-report-b2c.component.ts
+++ src/app/learner/pages/assessment-report-b2c/assessment-report-b2c.component.ts
@@ -7,16 +7,84 @@ import { Router } from '@angular/router';
   styleUrls: ['./assessment-report-b2c.component.scss']
 })
 export class AssessmentReportB2cComponent implements OnInit {
-  fromPage: any;
+  topicinfo: any;
+  SelfDuration = [
+    {
+      Week : '1',
+      Percentage : '64%',
+      WeeklyScore: '15',
+      isActive:true,
+      duration : {
+        estimated:"00:60:00",
+        spent:"00:50:00"
+      },
+      topics :{
+          total:"75",
+          completed:"50"
+      }
+    },
+    {
+      Week : '2',
+      Percentage : '51%',
+      WeeklyScore: '09',
+      isActive:false,
+      duration : {
+        estimated:"00:50:00",
+        spent:"00:40:00"
+      },
+      topics :{
+          total:"65",
+          completed:"40"
+      }
+    },
+    {
+      Week : '3',
+      Percentage : '61%',
+      WeeklyScore: '16',
+      isActive:false,
+      duration : {
+        estimated:"00:40:00",
+        spent:"00:30:00"
+      },
+      topics :{
+          total:"55",
+          completed:"30"
+      }
+    },
+    {
+      Week : '4',
+      Percentage : '48%',
+      WeeklyScore: '12',
+      isActive:false,
+      duration : {
+        estimated:"00:30:00",
+        spent:"00:20:00"
+      },
+      topics :{
+          total:"44",
+          completed:"20"
+      }
+    }
+  ]
   constructor(public route: Router) { }
 
   ngOnInit(): void {
+    this.topicinfo = this.SelfDuration[0];
+  }
+  
+  viewTopicDetails(item, selfDur) {
+    this.topicinfo = item;
+    selfDur.forEach(element => {
+      if(element.Week == item.Week) {
+        element.isActive = true;
+      } else {
+        element.isActive = false;
+      }
+    })
   }
 
   backButton() {
-    if (this.fromPage === 'mycourse') {
-      this.route.navigate(['/Landing/MyCourse']);
-    } 
+    this.route.navigate(['/Landing/MyCourse']);
   }
 
 }
